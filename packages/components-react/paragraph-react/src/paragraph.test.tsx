ab99de60b66d13de83dd6d23c5e94a130a53a7f9
matijs <matijs@gmail.com>
2024-10-09 16:05:04 +0200

    feat: add initial paragraph component
    - set up tsconfig files that extend from the root tsconfig.base.json
    - set up basic testing using jest and testing-library
    - show <Paragraph /> in Storybook

---
+++ packages/components-react/paragraph-react/src/paragraph.test.tsx
@@ -0,0 +1,12 @@
+import '@testing-library/jest-dom';
+import { render } from '@testing-library/react';
+import { Paragraph } from './paragraph';
+
+describe('Paragraph', () => {
+  it('renders a paragraph', () => {
+    const { container } = render(<Paragraph />);
+    const paragraph = container.querySelector(':only-child');
+
+    expect(paragraph).toBeInTheDocument();
+  });
+});
---
+++ packages/components-react/paragraph-react/src/paragraph.tsx
@@ -0,0 +1,31 @@
+import { clsx } from 'clsx';
+import { forwardRef } from 'react';
+import type { HTMLAttributes } from 'react';
+
+export interface ParagraphProps extends HTMLAttributes<HTMLParagraphElement> {
+  appearance?: 'lead' | 'small';
+}
+
+export const Paragraph = forwardRef<HTMLParagraphElement, ParagraphProps>(function Paragraph(props, forwardedRef) {
+  const { appearance, children, className, ...restProps } = props;
+
+  return (
+    <p
+      className={clsx(
+        'nl-paragraph',
+        { ['nl-paragraph--lead']: appearance === 'lead', ['nl-paragraph--small']: appearance === 'small' },
+        className,
+      )}
+      ref={forwardedRef}
+      {...restProps}
+    >
+      {appearance === 'lead' ? (
+        <b className="nl-paragraph__b">{children}</b>
+      ) : appearance === 'small' ? (
+        <small className="nl-paragraph__small">{children}</small>
+      ) : (
+        children
+      )}
+    </p>
+  );
+});
