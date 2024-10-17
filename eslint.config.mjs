a7076ad389f868afa40b04638c026ca1695979ce
matijs <matijs@gmail.com>
2024-10-17 20:25:08 +0200

    build: eslint configuration
    - Add eslint-plugin-react and eslint-plugin-jsx-a11y and use the
      recommended and jsx-runtime configurations and strict configuration on
      .tsx files only
    - Add @eslint/json and use the recommended configuration on .json files,
      treat tsconfig files as jsonc

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,23 +1,46 @@
 import globals from 'globals';
+import json from '@eslint/json';
 import js from '@eslint/js';
 import tseslint from 'typescript-eslint';
 import eslintConfigPrettier from 'eslint-config-prettier';
+import eslintPluginJsxA11y from 'eslint-plugin-jsx-a11y';
+import eslintPluginReact from 'eslint-plugin-react';
 
 export default tseslint.config(
   {
-    name: 'global-ignores',
-    ignores: ['**/dist/'],
+    ignores: ['**/dist/', '**/node_modules/'],
   },
   {
     languageOptions: {
-      globals: { ...globals.node },
-    },
-    linterOptions: {
-      reportUnusedDisableDirectives: true,
+      parserOptions: {
+        warnOnUnsupportedTypeScriptVersion: false,
+      },
     },
   },
-  js.configs.recommended,
+  {
+    files: ['**/*.js', '**/*.cjs', '**/*.mjs'],
+    languageOptions: {
+      globals: { ...globals.node },
+    },
+    ...js.configs.recommended,
+  },
+  {
+    files: ['**/*.json'],
+    language: 'json/json',
+    ...json.configs.recommended,
+  },
+  {
+    files: ['**/tsconfig.json', '**/tsconfig.*.json'],
+    language: 'json/jsonc',
+    ...json.configs.recommended,
+  },
   ...tseslint.configs.recommended,
+  {
+    files: ['**/*.tsx'],
+    ...eslintPluginReact.configs.flat.recommended,
+    ...eslintPluginReact.configs.flat['jsx-runtime'],
+    ...eslintPluginJsxA11y.flatConfigs.strict,
+  },
   {
     files: ['**/*.cjs'],
     rules: {
