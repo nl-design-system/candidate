01342427f313441ad0ec23284f6f23b26ea014c3
matijs <matijs@gmail.com>
2024-10-21 08:14:59 +0200

    chore: add @types/node to .ncurc.major.cjs
    Change `"//comment"` to `"//"`. See [this thread][1] for the reasoning
    why.
    
    [1]: https://groups.google.com/g/nodejs/c/NmL7jdeuw0M/m/yTqI05DRQrIJ

--- .ncurc.major.cjs
+++ .ncurc.major.cjs
@@ -2,6 +2,11 @@ const minorConfig = require('./.ncurc.minor.cjs');
 
 module.exports = {
   ...minorConfig,
-  reject: [...minorConfig.reject, 'eslint', 'eslint-plugin-json', 'style-dictionary'],
+  reject: [
+    ...minorConfig.reject,
+    // @types/node is kept in line with the node version in .nvmrc and package.json#engines.node
+    '@types/node',
+    'style-dictionary',
+  ],
   target: 'latest',
 };
