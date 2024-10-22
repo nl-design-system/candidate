57270154324e6aefad77ed6d0d21addfd2b68238
matijs <matijs@gmail.com>
2024-10-22 19:00:50 +0200

    feat: Heading 6
    - Add components-react/heading-6-react
    - Add components-css/heading-6-css

---
+++ packages/components-react/heading-6-react/src/heading-6.tsx
@@ -0,0 +1,15 @@
+import { clsx } from 'clsx';
+import type { HTMLAttributes } from 'react';
+import { forwardRef } from 'react';
+
+export type Heading6Props = HTMLAttributes<HTMLHeadingElement>;
+
+export const Heading6 = forwardRef<HTMLHeadingElement, Heading6Props>(function Heading6(props, forwardedRef) {
+  const { children, className, ...restProps } = props;
+
+  return (
+    <h6 {...restProps} className={clsx('nl-heading-6', className)} ref={forwardedRef}>
+      {children}
+    </h6>
+  );
+});
