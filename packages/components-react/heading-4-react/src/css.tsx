1c80598b9cecaf778a2d64b09b0a61f287c92de6
matijs <matijs@gmail.com>
2024-10-22 18:50:54 +0200

    feat: Heading 4
    - Add components-react/heading-4-react
    - Add components-css/heading-4-css

---
+++ packages/components-react/heading-4-react/src/css.tsx
@@ -0,0 +1,3 @@
+import '@nl-design-system-candidate/heading-4-css/heading-4.css';
+
+export { Heading4 } from './heading-4';
---
+++ packages/components-react/heading-4-react/src/heading-4.tsx
@@ -0,0 +1,15 @@
+import { clsx } from 'clsx';
+import type { HTMLAttributes } from 'react';
+import { forwardRef } from 'react';
+
+export type Heading4Props = HTMLAttributes<HTMLHeadingElement>;
+
+export const Heading4 = forwardRef<HTMLHeadingElement, Heading4Props>(function Heading4(props, forwardedRef) {
+  const { children, className, ...restProps } = props;
+
+  return (
+    <h4 {...restProps} className={clsx('nl-heading-4', className)} ref={forwardedRef}>
+      {children}
+    </h4>
+  );
+});
