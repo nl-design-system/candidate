a076163b7a937fbba28c7d6528ea3f0576075b3d
matijs <matijs@gmail.com>
2024-10-30 20:28:41 +0100

    build: use shared rollup config
    Reduce duplication of the rollup configuration by importing from a
    higher-level directory instead of repeating the same configuration
    across multiple packages.

--- packages/components-react/heading-1-react/rollup.config.mjs
+++ packages/components-react/heading-1-react/rollup.config.mjs
@@ -1,28 +1,7 @@
-import babel from '@rollup/plugin-babel';
-import nodeResolve from '@rollup/plugin-node-resolve';
-import typescript from '@rollup/plugin-typescript';
-import nodeExternals from 'rollup-plugin-node-externals';
-import peerDepsExternal from 'rollup-plugin-peer-deps-external';
-import postcss from 'rollup-plugin-postcss';
+import { defineConfig } from 'rollup';
+import { rollupConfig } from '../rollupConfig.mjs';
 
-export default {
+export default defineConfig({
   input: ['./src/heading-1.tsx', './src/css.tsx'],
-  output: {
-    dir: './dist/',
-    format: 'es',
-    sourcemap: true, // needed because of tsconfig.build.json
-  },
-  plugins: [
-    peerDepsExternal(),
-    nodeExternals(),
-    nodeResolve({ browser: true }),
-    typescript({ tsconfig: './tsconfig.build.json' }),
-    babel({
-      babelHelpers: 'runtime',
-      extensions: ['.ts', '.tsx'],
-      inputSourceMap: true,
-      plugins: ['@babel/plugin-transform-runtime'],
-    }),
-    postcss(),
-  ],
-};
+  ...rollupConfig,
+});
