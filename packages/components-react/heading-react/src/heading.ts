7c20f4d1b2f844f8621d402b240faa65bf089aa9
matijs <matijs@gmail.com>
2024-11-01 12:17:46 +0100

    feat: add Heading tests
    - Add `"test:watch"` script to conveniently retest while writing tests
    - Add tests
    - Create a `HeadingLevel` type out of `headlingLevels`, both are
      exported along with HeadingAppearance so they can be used in the tests

--- packages/components-react/heading-react/src/heading.ts
+++ packages/components-react/heading-react/src/heading.ts
@@ -2,14 +2,16 @@ import { clsx } from 'clsx';
 import type { HTMLAttributes } from 'react';
 import { forwardRef, createElement } from 'react';
 
-export type HeadingAppearance = 'level-1' | 'level-2' | 'level-3' | 'level-4' | 'level-5' | 'level-6';
+export const headingLevels = [1, 2, 3, 4, 5, 6] as const;
+
+export type HeadingLevel = (typeof headingLevels)[number];
+
+export type HeadingAppearance = `level-${HeadingLevel}`;
 
 export interface HeadingProps extends HTMLAttributes<HTMLHeadingElement> {
-  level: 1 | 2 | 3 | 4 | 5 | 6;
+  level: HeadingLevel;
   appearance?: HeadingAppearance;
 }
-
-const appearanceToHeading = (appearance: HeadingAppearance) => appearance.replace('level', 'nl-heading');
 
 export const Heading = forwardRef<HTMLHeadingElement, HeadingProps>(function Heading(props, forwardedRef) {
   const { level, appearance = `level-${level}`, className, children, ...restProps } = props;
@@ -17,7 +19,7 @@ export const Heading = forwardRef<HTMLHeadingElement, HeadingProps>(function Hea
 
   return createElement(
     type,
-    { className: clsx(appearanceToHeading(appearance), className), ref: forwardedRef, ...restProps },
+    { className: clsx('nl-heading', `nl-heading--${appearance}`, className), ref: forwardedRef, ...restProps },
     children,
   );
 });
