e373f543d1e9d6e78e7356083fae5975798c3bd5
matijs <matijs@gmail.com>
2024-11-01 12:17:46 +0100

    feat: add Heading tests
    - Add `"test:watch"` script to conveniently retest while writing tests
    - Add tests
    - Create a `HeadingLevel` type out of `headlingLevels`, both are
      exported along with HeadingAppearance so they can be used in the tests

--- packages/components-react/heading-react/src/heading.test.tsx
+++ packages/components-react/heading-react/src/heading.test.tsx
@@ -1,42 +1,107 @@
+import { describe, it } from '@jest/globals';
 import '@testing-library/jest-dom';
-import { render } from '@testing-library/react';
-import { Heading } from './heading';
+import { render, screen } from '@testing-library/react';
+import { createRef } from 'react';
+import type { HeadingAppearance } from './heading';
+import { Heading, headingLevels } from './heading';
 
-describe('Heading', () => {
-  it('renders a heading level 1', () => {
-    const { container } = render(<Heading level={1} />);
-    const heading = container.querySelector('h1');
+const extraClassName = 'extra-classname';
+const testId = 'rich-text';
 
-    expect(heading).toBeInTheDocument();
-  });
-  it('renders a heading level 2', () => {
-    const { container } = render(<Heading level={2} />);
-    const heading = container.querySelector('h2');
+headingLevels.forEach((level) => {
+  describe(`At heading level ${level}`, () => {
+    it('renders an element with role "heading"', () => {
+      render(<Heading level={level} />);
+      const heading = screen.getByRole('heading');
 
-    expect(heading).toBeInTheDocument();
-  });
-  it('renders a heading level 3', () => {
-    const { container } = render(<Heading level={3} />);
-    const heading = container.querySelector('h3');
+      expect(heading).toBeInTheDocument();
+    });
 
-    expect(heading).toBeInTheDocument();
-  });
-  it('renders a heading level 4', () => {
-    const { container } = render(<Heading level={4} />);
-    const heading = container.querySelector('h4');
+    it(`renders a heading at level ${level}`, () => {
+      render(<Heading level={level} />);
+      const heading = screen.getByRole('heading', { level });
 
-    expect(heading).toBeInTheDocument();
-  });
-  it('renders a heading level 5', () => {
-    const { container } = render(<Heading level={5} />);
-    const heading = container.querySelector('h5');
+      expect(heading).toBeInTheDocument();
+    });
 
-    expect(heading).toBeInTheDocument();
-  });
-  it('renders a heading level 6', () => {
-    const { container } = render(<Heading level={6} />);
-    const heading = container.querySelector('h6');
+    it(`renders an HTML h${level} element`, () => {
+      const { container } = render(<Heading level={level} />);
+      const heading = container.querySelector(`h${level}:only-child`);
 
-    expect(heading).toBeInTheDocument();
+      expect(heading).toBeInTheDocument();
+    });
+
+    it('renders an element with class name "nl-heading"', () => {
+      const { container } = render(<Heading level={level} />);
+      const heading = container.querySelector(`h${level}:only-child`);
+
+      expect(heading).toHaveClass('nl-heading');
+    });
+
+    it(`renders an element with class names "nl-heading" and "nl-heading--level-${level}"`, () => {
+      const { container } = render(<Heading level={level} />);
+      const heading = container.querySelector(`h${level}:only-child`);
+
+      expect(heading).toHaveClass('nl-heading', `nl-heading--level-${level}`);
+    });
+
+    it(`renders an element with role "heading" and name "Heading ${level}"`, () => {
+      const name = `Heading ${level}`;
+      render(<Heading level={level}>{name}</Heading>);
+      const heading = screen.getByRole('heading', { name });
+
+      expect(heading).toBeInTheDocument();
+    });
+
+    headingLevels.forEach((appearanceLevel) => {
+      it(`renders an HTML h${level} element with class names "nl-heading" and "nl-heading--level-${appearanceLevel}"`, () => {
+        const appearance = `level-${appearanceLevel}` as HeadingAppearance;
+        render(<Heading level={level} appearance={appearance} />);
+        const heading = screen.getByRole('heading', { level });
+
+        expect(heading).toHaveClass('nl-heading', `nl-heading--level-${appearanceLevel}`);
+      });
+    });
+
+    it(`supports hiding the HTML h${level} element visually and from the accessibility tree using the global HTML attribute "hidden"`, () => {
+      expect.assertions(2);
+      render(<Heading level={level} hidden />);
+      const heading = screen.getByRole('heading', { level, hidden: true });
+
+      try {
+        screen.getByRole('heading', { level });
+      } catch (e) {
+        expect(e).toBeDefined();
+      }
+      expect(heading).not.toBeVisible();
+    });
+
+    it(`renders an HTML h${level} element with an extra class name "${extraClassName}"`, () => {
+      render(<Heading level={level} className={extraClassName} />);
+      const heading = screen.getByRole('heading');
+
+      expect(heading).toHaveClass('nl-heading', `nl-heading--level-${level}`, extraClassName);
+    });
+
+    it('forwards React refs to the HTMLHeading heading element', () => {
+      const ref = createRef<HTMLHeadingElement>();
+      const { container } = render(<Heading level={level} ref={ref} />);
+      const heading = container.querySelector(':only-child');
+
+      expect(ref.current).toBe(heading);
+      expect(ref.current).toBeInstanceOf(HTMLHeadingElement);
+    });
+
+    it('renders rich text content', () => {
+      render(
+        <Heading level={level}>
+          <strong data-testid={testId}>strong</strong>
+        </Heading>,
+      );
+      const heading = screen.getByRole('heading', { level });
+      const richText = screen.getByTestId(testId);
+
+      expect(heading).toContainElement(richText);
+    });
   });
 });
