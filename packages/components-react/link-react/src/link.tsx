b3652fc8f9ed3cefc7f2d1f1019567fa06835d7b
Robbert Broersma <git@robbertbroersma.nl>
2024-11-09 09:50:45 +0100

    fix: make `href` required for `Link`

--- packages/components-react/link-react/src/link.tsx
+++ packages/components-react/link-react/src/link.tsx
@@ -2,7 +2,9 @@ import { clsx } from 'clsx';
 import type { AnchorHTMLAttributes } from 'react';
 import { forwardRef } from 'react';
 
-export type LinkProps = AnchorHTMLAttributes<HTMLAnchorElement>;
+export interface LinkProps extends AnchorHTMLAttributes<HTMLAnchorElement> {
+  href: AnchorHTMLAttributes<HTMLAnchorElement>['href'];
+}
 
 export const Link = forwardRef<HTMLAnchorElement, LinkProps>(function Link(props, forwardedRef) {
   const { children, className, ...restProps } = props;
