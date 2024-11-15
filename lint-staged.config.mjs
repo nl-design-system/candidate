c9424b75d5c09ad30377385b85f3c80387e727a6
matijs <matijs@gmail.com>
2024-11-15 19:14:51 +0100

    build: update lint-staged config
    Try to fix linting issues just before committing. Failing to fix will
    still result in the commit not going through.

--- lint-staged.config.mjs
+++ lint-staged.config.mjs
@@ -1,6 +1,6 @@
 export default {
-  '*.{css,scss}': ['stylelint --allow-empty-input', 'prettier --write'],
-  '*.{js,cjs,mjs,json,jsx,mdx,ts,tsx}': ['eslint --no-error-on-unmatched-pattern', 'prettier --write'],
+  '*.{css,scss}': ['stylelint --allow-empty-input --fix', 'prettier --write'],
+  '*.{js,cjs,mjs,json,jsx,mdx,ts,tsx}': ['eslint --no-error-on-unmatched-pattern --fix', 'prettier --write'],
   '*.{yml,yaml}': ['prettier --write'],
   '*.md': ['markdownlint', 'prettier --write'],
   'package.json': 'npmPkgJsonLint --allowEmptyTargets',
