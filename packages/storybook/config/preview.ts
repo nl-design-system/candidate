6f9107cedd5b22ac92861f19ad8b5aa20bc1135d
matijs <matijs@gmail.com>
2024-11-15 14:07:53 +0100

    feat: fine tune Storybook
    - Move stories to ./stories
    - Move MDX docs to ./docs
    - Move custom Storybook components to ./src
    - Clean up the Paragraph stories:
      - Add external links to Figma and GitHub
      - Remove `args` from `meta` and make them explicit for every story
      - Add names where applicable

--- packages/storybook/config/preview.ts
+++ packages/storybook/config/preview.ts
@@ -1,42 +1,25 @@
-import type { Preview } from '@storybook/react';
-import { ParametersArgsDecorator } from './ParametersArgsDecorator';
 import '@nl-design-system-unstable/voorbeeld-design-tokens/dist/theme.css';
 import '@nl-design-system-unstable/voorbeeld-design-tokens/src/font';
+import type { Preview } from '@storybook/react';
+import { DocsPage } from '../src/DocsPage';
+import { ParametersArgsDecorator } from '../src/ParametersArgsDecorator';
 
 const preview: Preview = {
   decorators: [ParametersArgsDecorator],
   parameters: {
     controls: { expanded: false },
-    options: { panelPosition: 'right' },
-    status: {
-      statuses: {
-        ALPHA: {
-          background: '#e0bc2e',
-          color: '#000000',
-          description:
-            'Used in prototypes and in projects that are still in development, breaking changes occur frequently and are not communicated.',
-        },
-        BETA: {
-          background: '#3065ee',
-          color: '#ffffff',
-          description:
-            'Used in production in a specific situation, evolving APIs based on feedback, breaking changes are still likely.',
-        },
-        PRODUCTION: {
-          background: '#088008',
-          color: '#ffffff',
-          description:
-            'Used in production in a variety of situations, well tested, stable APIs, mostly patches and minor releases.',
-        },
-        'WORK IN PROGRESS': {
-          background: '#cc0000',
-          color: '#ffffff',
-          description:
-            'Do not use in production. Does not follow semantic versioning and any published packages are for internal use only.',
-        },
-      },
+    docs: {
+      // page template for automatic docs
+      page: DocsPage,
+    },
+    // @whitespace/storybook-addon-html
+    html: {
+      root: '.voorbeeld-theme', // use `<div class="voorbeeld-theme"> as the root
     },
   },
+  tags: [
+    'autodocs', // enable automatic docs generation for all stories
+  ],
 };
 
 export default preview;
