74a1c3cbf1f61561b097014281df18c73cb9b032
Robbert Broersma <git@robbertbroersma.nl>
2024-11-19 20:21:16 +0100

    ci: require 100% code coverage

--- packages/components-react/code-block-react/jest.config.mjs
+++ packages/components-react/code-block-react/jest.config.mjs
@@ -3,5 +3,13 @@
  * */
 export default {
   coverageProvider: 'v8',
+  coverageThreshold: {
+    global: {
+      branches: 100,
+      functions: 100,
+      lines: 100,
+      statements: 100,
+    },
+  },
   testEnvironment: 'jest-environment-jsdom',
 };
--- packages/components-react/heading-1-react/jest.config.mjs
+++ packages/components-react/heading-1-react/jest.config.mjs
@@ -1,7 +1,18 @@
 /**
  * @type {import('jest').Config}
  * */
+/**
+ * @type {import('jest').Config}
+ * */
 export default {
   coverageProvider: 'v8',
+  coverageThreshold: {
+    global: {
+      branches: 100,
+      functions: 100,
+      lines: 100,
+      statements: 100,
+    },
+  },
   testEnvironment: 'jest-environment-jsdom',
 };
