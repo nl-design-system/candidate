9ea7cf5d95437001b9ae63c543f31893b9f7590c
aakminasi <arefcoding@gmail.com>
2024-11-27 15:59:23 +0100

    data-badge

--- packages/storybook-test/stories/data-badge.stories.tsx
+++ packages/storybook-test/stories/data-badge.stories.tsx
@@ -2,6 +2,9 @@ import type { Meta, StoryObj } from '@storybook/react';
 import packageJSON from '../../components-react/data-badge-react/package.json';
 import { DataBadge } from '../../components-react/data-badge-react/src/css';
 
+const StoryDate = new Date();
+const ISODate = StoryDate.toISOString();
+
 const meta = {
   argTypes: {
     children: { table: { category: 'API' }, type: 'string' },
@@ -21,7 +24,7 @@ const meta = {
       },
     ],
   },
-  title: 'Componenten/Data Badge',
+  title: 'Data Badge',
 } satisfies Meta<typeof DataBadge>;
 
 export default meta;
@@ -29,24 +32,105 @@ export default meta;
 type Story = StoryObj<typeof meta>;
 
 export const Default: Story = {
-  name: 'Data Badge',
+  name: 'Default',
   args: {
-    children: '42',
+    children: 'Onderwijs',
+    lang: 'nl',
   },
 };
 
-export const DataBadgeWithValue: Story = {
-  name: 'Data Badge met "value"',
+export const DefaultTranslationEn: Story = {
+  name: 'Default vertaling Engels',
   args: {
-    children: '42',
-    value: '42',
+    children: 'Education',
+    lang: 'en',
   },
 };
 
-export const DataBadgeWithDateTime: Story = {
-  name: 'Data Badge met "dateTime"',
+export const DefaultTranslationGer: Story = {
+  name: 'Default vertaling Duits',
   args: {
-    children: 'Donderdag 1 januari 1970 om 01:00:00',
-    dateTime: '1970-01-01T00:00:00+01:00',
+    children: 'Ausbildung',
+    lang: 'de',
+  },
+};
+
+export const RightToLeft: Story = {
+  name: 'Right To Left',
+  args: {
+    children: 'حدد خياراً',
+    dir: 'rtl',
+  },
+};
+
+export const DateNL: Story = {
+  name: 'Datumweergave Nederland',
+  args: {
+    children: StoryDate.toLocaleDateString('nl-Nl'),
+    dateTime: ISODate,
+  },
+};
+
+export const DateUS: Story = {
+  name: 'Datumweergave Verenigde Staten',
+  args: {
+    children: StoryDate.toLocaleDateString('us-Us'),
+    dateTime: ISODate,
+  },
+};
+
+export const TimeNL: Story = {
+  name: 'Tijdweergave Nederland',
+  args: {
+    children: StoryDate.toLocaleTimeString('nl-Nl'),
+    dateTime: ISODate,
+  },
+};
+
+export const TimeUS: Story = {
+  name: 'Tijdweergave Verenigde Staten',
+  args: {
+    children: StoryDate.toLocaleTimeString('us-Us'),
+    dateTime: ISODate,
+  },
+};
+
+export const shortDateNl: Story = {
+  name: 'Shortdate Nederlands',
+  args: {
+    children: 'vrijdag 18 augustus',
+    dateTime: '2023-08-18',
+  },
+};
+
+export const shortDateEn: Story = {
+  name: 'Shortdate Engels',
+  args: {
+    children: 'Friday 18th August',
+    dateTime: '2023-08-18',
+  },
+};
+
+export const MachineReadableDuration: Story = {
+  name: 'Machine Readable Duration',
+  args: {
+    children: '1 uur en 39 minuten',
+    dateTime: ISODate,
+  },
+};
+
+export const EdgeCaseNl: Story = {
+  name: 'Edge Case Nederlands',
+  args: {
+    children: 'Samenlevingscontract',
+    lang: 'nl',
+  },
+};
+
+export const EdgeCaseGer: Story = {
+  name: 'Edge Case Duits',
+  args: {
+    children: 'Lebenspartnerschaftsvertrag',
+    lang: 'de',
   },
 };
