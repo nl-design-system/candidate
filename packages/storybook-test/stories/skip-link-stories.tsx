610d24d938795dd001a32560ea26aae7bedbdc73
matijs <matijs@gmail.com>
2024-12-04 14:11:56 +0100

    feat: add Skip Link CSS
    - Add styling for the Skip Link component
    - Add Storybook-specific styling in `_storybook.scss`. This file is only
      included in the component stories file. It is not part of `./src` in
      the published package and because it has a leading underscore it is
      not built separately by Sass itself
    - Add an extra story to demonstrate a fake `focus-visible` for the Skip
      Link component

--- packages/storybook-test/stories/skip-link-stories.tsx
+++ packages/storybook-test/stories/skip-link-stories.tsx
@@ -1,4 +1,6 @@
 import type { Meta, StoryObj } from '@storybook/react';
+import { Heading } from '../../components-react/heading-react/src/css';
+import { Paragraph } from '../../components-react/paragraph-react/src/css';
 import packageJSON from '../../components-react/skip-link-react/package.json';
 import { SkipLink } from '../../components-react/skip-link-react/src/css';
 
@@ -25,6 +27,22 @@ const meta = {
       },
     ],
   },
+  render(props) {
+    return (
+      <>
+        <SkipLink {...props} />
+        <main id={props.href?.substring(1)}>
+          <Heading level={1}>Test inhoud</Heading>
+          <Paragraph>
+            Wij hebben er heel wat rondgezworven, maar zijn er zeker van, dat we nog menig mooi en belangrijk plekje
+            gemist hebben. Dat is maar goed ook; nu hebben wij een reden, om nog dikwijls terug te keeren naar de
+            plaatsen, waar het ons zoo goed bevallen is en waar wij nu nog nieuwe en frissche dingen kunnen ontdekken.
+            Indien gij daar ook aan wilt doen, zult ge het u niet beklagen.
+          </Paragraph>
+        </main>
+      </>
+    );
+  },
   title: 'Componenten/Skip Link',
 } satisfies Meta<typeof SkipLink>;
 
@@ -36,7 +54,23 @@ export const Default: Story = {
   name: 'Skip Link',
   args: {
     children: 'naar de inhoud',
-    href: '#',
+    href: '#main-story-1',
+  },
+  parameters: {
+    docs: {
+      description: {
+        story: 'Story beschrijving',
+      },
+    },
+  },
+};
+
+export const FocusedSkipLink: Story = {
+  name: 'Skip Link (gefocust)',
+  args: {
+    children: 'naar de inhoud',
+    className: 'nl-skip-link--storybook-focus-visible',
+    href: '#main-story-2',
   },
   parameters: {
     docs: {
