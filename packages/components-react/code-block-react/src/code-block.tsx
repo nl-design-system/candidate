24316b3a95c8b07978aa0bc5d36ef8ebeabd8d8d
matijs <matijs@gmail.com>
2024-12-10 14:12:31 +0100

    feat: Code Block CSS
    - rename `code-block__content` -> `code-block__code`
    - add `dir="ltr"` to the Code block but do allow it to be overwritten to
      `undefined` by spreading rest props _after_ `dir`
    - update tests
    - add stories and non-conforming stories

--- packages/components-react/code-block-react/src/code-block.tsx
+++ packages/components-react/code-block-react/src/code-block.tsx
@@ -8,8 +8,8 @@ export const CodeBlock = forwardRef<HTMLPreElement, CodeBlockProps>(function Cod
   const { children, className, ...restProps } = props;
 
   return (
-    <pre {...restProps} className={clsx('nl-code-block', className)} ref={forwardedRef}>
-      <code className="nl-code-block__content">{children}</code>
+    <pre dir="ltr" {...restProps} className={clsx('nl-code-block', className)} ref={forwardedRef}>
+      <code className="nl-code-block__code">{children}</code>
     </pre>
   );
 });
--- packages/storybook-test/stories/code-block/code-block.stories.tsx
+++ packages/storybook-test/stories/code-block/code-block.stories.tsx
@@ -1,6 +1,7 @@
 import type { Meta, StoryObj } from '@storybook/react';
 import packageJSON from '../../../components-react/code-block-react/package.json';
 import { CodeBlock } from '../../../components-react/code-block-react/src/css';
+import { Paragraph } from '../../../components-react/paragraph-react/src/css';
 import componentMarkdown from '../../../docs/code-block-docs/docs/component.md?raw';
 import tokens from '../../../tokens/code-block-tokens/tokens.json';
 
@@ -41,5 +42,121 @@ export const Default: Story = {
   },
   parameters: {
     status: { type: [] },
+  },
+};
+
+export const KorteRegel: Story = {
+  name: 'Korte regel',
+  args: {
+    children: `npm install`,
+  },
+};
+
+export const LangeRegel: Story = {
+  name: 'Lange regel',
+  args: {
+    children: `class ProjectContractChargingPeriodProjectAccountReferenceVM extends AbstractProjectContractChargingPeriodProjectAccountReferenceVM implements ProjectContractChargingPeriodProjectAccountReferenceVMInterface {
+    /* todo */
+}`,
+  },
+  parameters: {
+    docs: {
+      description: {
+        story:
+          'De volledige inhoud van de code block moet leesbaar zijn, ook als de content heel lange regels bevat. De code block moet horizontaal kunnen scrollen.',
+      },
+    },
+  },
+};
+
+export const DefaultFont: Story = {
+  name: 'Default font',
+  args: {
+    children: `De Code Block moet visueel onderscheidbaar zijn.`,
+  },
+  parameters: {
+    docs: {
+      description: {
+        story: 'De Code Block moet visueel onderscheidbaar zijn, ook als er geen font design token is ingesteld.',
+      },
+    },
+  },
+};
+
+export const FallbackFont: Story = {
+  name: 'Fallback font',
+  args: {
+    children: `De Code Block moet visueel onderscheidbaar zijn.`,
+    style: {
+      '--nl-code-block-font-family': '""',
+    } as CSSProperties,
+  },
+  parameters: {
+    docs: {
+      description: {
+        story: `De Code Block moet visueel onderscheidbaar zijn, ook wanneer de in de design token ingestelde code font niet geladen kan worden.`,
+      },
+    },
+  },
+};
+
+export const Nederlands: Story = {
+  name: 'Code met Nederlandse taal',
+  args: {
+    children: `// In dit voorbeeld berekenen we 7 keer 6.
+var nummerA = 7;
+var nummerB = 6;
+var antwoord = nummerA * nummerB;`,
+    lang: 'nl',
+  },
+  parameters: {
+    docs: {
+      description: {
+        story: 'Een Code Block met een codevoorbeeld voor Nederlandstalige lezers.',
+      },
+    },
+  },
+};
+
+export const Engels: Story = {
+  name: 'Code met Engelse taal',
+  args: {
+    children: `// In this example we'll calculate 7 times 6.
+var numberA = 7;
+var numberB = 6;
+var answer = numberA * numberB;`,
+    lang: 'en',
+  },
+  parameters: {
+    docs: {
+      description: {
+        story: 'Een Code Block met een codevoorbeeld voor Engelstalig lezers.',
+      },
+    },
+  },
+};
+
+export const ArabicContext: Story = {
+  name: 'Code Block in Engelse taal geplaatst binnen Arabische rechts-naar-links tekst',
+  args: {
+    children: `var numberA = 7;
+var numberB = 6;
+var answer = numberA * numberB;`,
+    lang: 'en',
+  },
+  decorators: [
+    (Story) => (
+      <div lang="ar" dir="rtl">
+        <Paragraph>في المثال التالي سوف نحسب 7 مرات 6</Paragraph>
+        {Story()}
+      </div>
+    ),
+  ],
+  parameters: {
+    docs: {
+      description: {
+        story: 'Een Code Block met een codevoorbeeld voor Engelstalig lezers.',
+      },
+    },
   },
 };
