73e48faa11c260d699baa7a3fc7ea43b06195500
Robbert Broersma <git@robbertbroersma.nl>
2024-12-11 00:49:35 +0100

    docs: improve Paragraph stories

--- packages/storybook-test/config/viewports.ts
+++ packages/storybook-test/config/viewports.ts
@@ -25,4 +25,28 @@ export const viewports: ViewportMap = {
     },
     type: 'tablet',
   },
+  wcag100: {
+    name: 'WCAG 100%',
+    styles: {
+      height: '1024px',
+      width: '1280px',
+    },
+    type: 'other',
+  },
+  wcag200: {
+    name: 'WCAG 200%',
+    styles: {
+      height: '512px',
+      width: '640px',
+    },
+    type: 'other',
+  },
+  wcag400: {
+    name: 'WCAG 400%',
+    styles: {
+      height: '256px',
+      width: '320px',
+    },
+    type: 'other',
+  },
 };
---
+++ packages/storybook-test/src/TextDecorator.tsx
@@ -0,0 +1,47 @@
+import { Decorator } from '@storybook/react';
+
+/* eslint-disable react/display-name */
+
+export const LargeLetterSpacingDecorator: Decorator = (Story) => (
+  <div
+    style={{
+      letterSpacing: '0.12em',
+    }}
+  >
+    {Story()}
+  </div>
+);
+
+export const LargeWordSpacingDecorator: Decorator = (Story) => (
+  <div
+    style={{
+      wordSpacing: '0.16em',
+    }}
+  >
+    {Story()}
+  </div>
+);
+
+const createRootFontSizeDecorator =
+  ({ fontSize, lineHeight }: { fontSize?: string; lineHeight?: string }): Decorator =>
+  (Story) => (
+    <>
+      <style
+        dangerouslySetInnerHTML={{
+          __html: [
+            ':root {',
+            fontSize ? `font-size: ${fontSize};` : '',
+            lineHeight ? `line-height: ${lineHeight};` : '',
+            '}',
+          ].join(' '),
+        }}
+      ></style>
+      {Story()}
+    </>
+  );
+
+export const LargeLineHeightDecorator = createRootFontSizeDecorator({ lineHeight: '48px' });
+
+export const UserPreference2rem = createRootFontSizeDecorator({ fontSize: '32px' });
+
+export const UserPreference4rem = createRootFontSizeDecorator({ fontSize: '64px' });
