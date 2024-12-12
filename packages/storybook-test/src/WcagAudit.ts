69128306535482ec6787627e73c90ca0214532a4
Robbert Broersma <git@robbertbroersma.nl>
2024-12-12 09:27:06 +0100

    docs: add WCAG evaluation

---
+++ packages/storybook-test/src/WcagAudit.ts
@@ -0,0 +1,369 @@
+/** Success criterium 1.1.1 */
+export const WCAG22_NON_TEXT_CONTENT = 'https://www.w3.org/TR/WCAG22/#non-text-content';
+
+/** Success criterium 1.2.1 */
+export const WCAG22_AUDIO_ONLY_AND_VIDEO_ONLY_PRERECORDED =
+  'https://www.w3.org/TR/WCAG22/#audio-only-and-video-only-prerecorded';
+
+/** Success criterium 1.2.2 */
+export const WCAG22_CAPTIONS_PRERECORDED = 'https://www.w3.org/TR/WCAG22/#captions-prerecorded';
+
+/** Success criterium 1.2.3 */
+export const WCAG22_AUDIO_DESCRIPTION_OR_MEDIA_ALTERNATIVE_PRERECORDED =
+  'https://www.w3.org/TR/WCAG22/#audio-description-or-media-alternative-prerecorded';
+
+/** Success criterium 1.2.4 */
+export const WCAG22_CAPTIONS_LIVE = 'https://www.w3.org/TR/WCAG22/#captions-live';
+
+/** Success criterium 1.2.5 */
+export const WCAG22_AUDIO_DESCRIPTION_PRERECORDED = 'https://www.w3.org/TR/WCAG22/#audio-description-prerecorded';
+
+/** Success criterium 1.2.6 */
+export const WCAG22_SIGN_LANGUAGE_PRERECORDED = 'https://www.w3.org/TR/WCAG22/#sign-language-prerecorded';
+
+/** Success criterium 1.2.7 */
+export const WCAG22_EXTENDED_AUDIO_DESCRIPTION_PRERECORDED =
+  'https://www.w3.org/TR/WCAG22/#extended-audio-description-prerecorded';
+
+/** Success criterium 1.2.8 */
+export const WCAG22_MEDIA_ALTERNATIVE_PRERECORDED = 'https://www.w3.org/TR/WCAG22/#media-alternative-prerecorded';
+
+/** Success criterium 1.2.9 */
+export const WCAG22_AUDIO_ONLY_LIVE = 'https://www.w3.org/TR/WCAG22/#audio-only-live';
+
+/** Success criterium 1.3.1 */
+export const WCAG22_INFO_AND_RELATIONSHIPS = 'https://www.w3.org/TR/WCAG22/#info-and-relationships';
+
+/** Success criterium 1.3.2 */
+export const WCAG22_MEANINGFUL_SEQUENCE = 'https://www.w3.org/TR/WCAG22/#meaningful-sequence';
+
+/** Success criterium 1.3.3 */
+export const WCAG22_SENSORY_CHARACTERISTICS = 'https://www.w3.org/TR/WCAG22/#sensory-characteristics';
+
+/** Success criterium 1.3.4 */
+export const WCAG22_ORIENTATION = 'https://www.w3.org/TR/WCAG22/#orientation';
+
+/** Success criterium 1.3.5 */
+export const WCAG22_IDENTIFY_INPUT_PURPOSE = 'https://www.w3.org/TR/WCAG22/#identify-input-purpose';
+
+/** Success criterium 1.3.6 */
+export const WCAG22_IDENTIFY_PURPOSE = 'https://www.w3.org/TR/WCAG22/#identify-purpose';
+
+/** Success criterium 1.4.1 */
+export const WCAG22_USE_OF_COLOR = 'https://www.w3.org/TR/WCAG22/#use-of-color';
+
+/** Success criterium 1.4.2 */
+export const WCAG22_AUDIO_CONTROL = 'https://www.w3.org/TR/WCAG22/#audio-control';
+
+/** Success criterium 1.4.3 */
+export const WCAG22_CONTRAST_MINIMUM = 'https://www.w3.org/TR/WCAG22/#contrast-minimum';
+
+/** Success criterium 1.4.4 */
+export const WCAG22_RESIZE_TEXT = 'https://www.w3.org/TR/WCAG22/#resize-text';
+
+/** Success criterium 1.4.5 */
+export const WCAG22_IMAGES_OF_TEXT = 'https://www.w3.org/TR/WCAG22/#images-of-text';
+
+/** Success criterium 1.4.6 */
+export const WCAG22_CONTRAST_ENHANCED = 'https://www.w3.org/TR/WCAG22/#contrast-enhanced';
+
+/** Success criterium 1.4.7 */
+export const WCAG22_LOW_OR_NO_BACKGROUND_AUDIO = 'https://www.w3.org/TR/WCAG22/#low-or-no-background-audio';
+
+/** Success criterium 1.4.8 */
+export const WCAG22_VISUAL_PRESENTATION = 'https://www.w3.org/TR/WCAG22/#visual-presentation';
+
+/** Success criterium 1.4.9 */
+export const WCAG22_IMAGES_OF_TEXT_NO_EXCEPTION = 'https://www.w3.org/TR/WCAG22/#images-of-text-no-exception';
+
+/** Success criterium 1.4.10 */
+export const WCAG22_REFLOW = 'https://www.w3.org/TR/WCAG22/#reflow';
+
+/** Success criterium 1.4.11 */
+export const WCAG22_NON_TEXT_CONTRAST = 'https://www.w3.org/TR/WCAG22/#non-text-contrast';
+
+/** Success criterium 1.4.12 */
+export const WCAG22_TEXT_SPACING = 'https://www.w3.org/TR/WCAG22/#text-spacing';
+
+/** Success criterium 1.4.13 */
+export const WCAG22_CONTENT_ON_HOVER_OR_FOCUS = 'https://www.w3.org/TR/WCAG22/#content-on-hover-or-focus';
+
+/** Success criterium 2.1.1 */
+export const WCAG22_KEYBOARD = 'https://www.w3.org/TR/WCAG22/#keyboard';
+
+/** Success criterium 2.1.2 */
+export const WCAG22_NO_KEYBOARD_TRAP = 'https://www.w3.org/TR/WCAG22/#no-keyboard-trap';
+
+/** Success criterium 2.1.3 */
+export const WCAG22_KEYBOARD_NO_EXCEPTION = 'https://www.w3.org/TR/WCAG22/#keyboard-no-exception';
+
+/** Success criterium 2.1.4 */
+export const WCAG22_CHARACTER_KEY_SHORTCUTS = 'https://www.w3.org/TR/WCAG22/#character-key-shortcuts';
+
+/** Success criterium 2.2.1 */
+export const WCAG22_TIMING_ADJUSTABLE = 'https://www.w3.org/TR/WCAG22/#timing-adjustable';
+
+/** Success criterium 2.2.2 */
+export const WCAG22_PAUSE_STOP_HIDE = 'https://www.w3.org/TR/WCAG22/#pause-stop-hide';
+
+/** Success criterium 2.2.3 */
+export const WCAG22_NO_TIMING = 'https://www.w3.org/TR/WCAG22/#no-timing';
+
+/** Success criterium 2.2.4 */
+export const WCAG22_INTERRUPTIONS = 'https://www.w3.org/TR/WCAG22/#interruptions';
+
+/** Success criterium 2.2.5 */
+export const WCAG22_RE_AUTHENTICATING = 'https://www.w3.org/TR/WCAG22/#re-authenticating';
+
+/** Success criterium 2.2.6 */
+export const WCAG22_TIMEOUTS = 'https://www.w3.org/TR/WCAG22/#timeouts';
+
+/** Success criterium 2.3.1 */
+export const WCAG22_THREE_FLASHES_OR_BELOW_THRESHOLD = 'https://www.w3.org/TR/WCAG22/#three-flashes-or-below-threshold';
+
+/** Success criterium 2.3.2 */
+export const WCAG22_THREE_FLASHES = 'https://www.w3.org/TR/WCAG22/#three-flashes';
+
+/** Success criterium 2.3.3 */
+export const WCAG22_ANIMATION_FROM_INTERACTIONS = 'https://www.w3.org/TR/WCAG22/#animation-from-interactions';
+
+/** Success criterium 2.4.1 */
+export const WCAG22_BYPASS_BLOCKS = 'https://www.w3.org/TR/WCAG22/#bypass-blocks';
+
+/** Success criterium 2.4.2 */
+export const WCAG22_PAGE_TITLED = 'https://www.w3.org/TR/WCAG22/#page-titled';
+
+/** Success criterium 2.4.3 */
+export const WCAG22_FOCUS_ORDER = 'https://www.w3.org/TR/WCAG22/#focus-order';
+
+/** Success criterium 2.4.4 */
+export const WCAG22_LINK_PURPOSE_IN_CONTEXT = 'https://www.w3.org/TR/WCAG22/#link-purpose-in-context';
+
+/** Success criterium 2.4.5 */
+export const WCAG22_MULTIPLE_WAYS = 'https://www.w3.org/TR/WCAG22/#multiple-ways';
+
+/** Success criterium 2.4.6 */
+export const WCAG22_HEADINGS_AND_LABELS = 'https://www.w3.org/TR/WCAG22/#headings-and-labels';
+
+/** Success criterium 2.4.7 */
+export const WCAG22_FOCUS_VISIBLE = 'https://www.w3.org/TR/WCAG22/#focus-visible';
+
+/** Success criterium 2.4.8 */
+export const WCAG22_LOCATION = 'https://www.w3.org/TR/WCAG22/#location';
+
+/** Success criterium 2.4.9 */
+export const WCAG22_LINK_PURPOSE_LINK_ONLY = 'https://www.w3.org/TR/WCAG22/#link-purpose-link-only';
+
+/** Success criterium 2.4.10 */
+export const WCAG22_SECTION_HEADINGS = 'https://www.w3.org/TR/WCAG22/#section-headings';
+
+/** Success criterium 2.5.1 */
+export const WCAG22_POINTER_GESTURES = 'https://www.w3.org/TR/WCAG22/#pointer-gestures';
+
+/** Success criterium 2.5.2 */
+export const WCAG22_POINTER_CANCELLATION = 'https://www.w3.org/TR/WCAG22/#pointer-cancellation';
+
+/** Success criterium 2.5.3 */
+export const WCAG22_LABEL_IN_NAME = 'https://www.w3.org/TR/WCAG22/#label-in-name';
+
+/** Success criterium 2.5.4 */
+export const WCAG22_MOTION_ACTUATION = 'https://www.w3.org/TR/WCAG22/#motion-actuation';
+
+/** Success criterium 2.5.5 */
+export const WCAG22_TARGET_SIZE = 'https://www.w3.org/TR/WCAG22/#target-size';
+
+/** Success criterium 2.5.6 */
+export const WCAG22_CONCURRENT_INPUT_MECHANISMS = 'https://www.w3.org/TR/WCAG22/#concurrent-input-mechanisms';
+
+/** Success criterium 3.1.1 */
+export const WCAG22_LANGUAGE_OF_PAGE = 'https://www.w3.org/TR/WCAG22/#language-of-page';
+
+/** Success criterium 3.1.2 */
+export const WCAG22_LANGUAGE_OF_PARTS = 'https://www.w3.org/TR/WCAG22/#language-of-parts';
+
+/** Success criterium 3.1.3 */
+export const WCAG22_UNUSUAL_WORDS = 'https://www.w3.org/TR/WCAG22/#unusual-words';
+
+/** Success criterium 3.1.4 */
+export const WCAG22_ABBREVIATIONS = 'https://www.w3.org/TR/WCAG22/#abbreviations';
+
+/** Success criterium 3.1.5 */
+export const WCAG22_READING_LEVEL = 'https://www.w3.org/TR/WCAG22/#reading-level';
+
+/** Success criterium 3.1.6 */
+export const WCAG22_PRONUNCIATION = 'https://www.w3.org/TR/WCAG22/#pronunciation';
+
+/** Success criterium 3.2.1 */
+export const WCAG22_ON_FOCUS = 'https://www.w3.org/TR/WCAG22/#on-focus';
+
+/** Success criterium 3.2.2 */
+export const WCAG22_ON_INPUT = 'https://www.w3.org/TR/WCAG22/#on-input';
+
+/** Success criterium 3.2.3 */
+export const WCAG22_CONSISTENT_NAVIGATION = 'https://www.w3.org/TR/WCAG22/#consistent-navigation';
+
+/** Success criterium 3.2.4 */
+export const WCAG22_CONSISTENT_IDENTIFICATION = 'https://www.w3.org/TR/WCAG22/#consistent-identification';
+
+/** Success criterium 3.2.5 */
+export const WCAG22_CHANGE_ON_REQUEST = 'https://www.w3.org/TR/WCAG22/#change-on-request';
+
+/** Success criterium 3.3.1 */
+export const WCAG22_ERROR_IDENTIFICATION = 'https://www.w3.org/TR/WCAG22/#error-identification';
+
+/** Success criterium 3.3.2 */
+export const WCAG22_LABELS_OR_INSTRUCTIONS = 'https://www.w3.org/TR/WCAG22/#labels-or-instructions';
+
+/** Success criterium 3.3.3 */
+export const WCAG22_ERROR_SUGGESTION = 'https://www.w3.org/TR/WCAG22/#error-suggestion';
+
+/** Success criterium 3.3.4 */
+export const WCAG22_ERROR_PREVENTION_LEGAL_FINANCIAL_DATA =
+  'https://www.w3.org/TR/WCAG22/#error-prevention-legal-financial-data';
+
+/** Success criterium 3.3.5 */
+export const WCAG22_HELP = 'https://www.w3.org/TR/WCAG22/#help';
+
+/** Success criterium 3.3.6 */
+export const WCAG22_ERROR_PREVENTION_ALL = 'https://www.w3.org/TR/WCAG22/#error-prevention-all';
+
+/** Success criterium 4.1.1 */
+export const WCAG22_PARSING = 'https://www.w3.org/TR/WCAG22/#parsing';
+
+/** Success criterium 4.1.2 */
+export const WCAG22_NAME_ROLE_VALUE = 'https://www.w3.org/TR/WCAG22/#name-role-value';
+
+/** Success criterium 4.1.3 */
+export const WCAG22_STATUS_MESSAGES = 'https://www.w3.org/TR/WCAG22/#status-messages';
+
+export const WCAG22_AA_LEVEL = [
+  WCAG22_NON_TEXT_CONTENT,
+  WCAG22_AUDIO_ONLY_AND_VIDEO_ONLY_PRERECORDED,
+  WCAG22_CAPTIONS_PRERECORDED,
+  WCAG22_AUDIO_DESCRIPTION_OR_MEDIA_ALTERNATIVE_PRERECORDED,
+  WCAG22_CAPTIONS_LIVE,
+  WCAG22_AUDIO_DESCRIPTION_PRERECORDED,
+  WCAG22_INFO_AND_RELATIONSHIPS,
+  WCAG22_MEANINGFUL_SEQUENCE,
+  WCAG22_SENSORY_CHARACTERISTICS,
+  WCAG22_ORIENTATION,
+  WCAG22_IDENTIFY_INPUT_PURPOSE,
+  WCAG22_USE_OF_COLOR,
+  WCAG22_AUDIO_CONTROL,
+  WCAG22_CONTRAST_MINIMUM,
+  WCAG22_RESIZE_TEXT,
+  WCAG22_IMAGES_OF_TEXT,
+  WCAG22_REFLOW,
+  WCAG22_NON_TEXT_CONTRAST,
+  WCAG22_TEXT_SPACING,
+  WCAG22_CONTENT_ON_HOVER_OR_FOCUS,
+  WCAG22_KEYBOARD,
+  WCAG22_NO_KEYBOARD_TRAP,
+  WCAG22_CHARACTER_KEY_SHORTCUTS,
+  WCAG22_TIMING_ADJUSTABLE,
+  WCAG22_PAUSE_STOP_HIDE,
+  WCAG22_THREE_FLASHES_OR_BELOW_THRESHOLD,
+  WCAG22_BYPASS_BLOCKS,
+  WCAG22_PAGE_TITLED,
+  WCAG22_FOCUS_ORDER,
+  WCAG22_LINK_PURPOSE_IN_CONTEXT,
+  WCAG22_MULTIPLE_WAYS,
+  WCAG22_HEADINGS_AND_LABELS,
+  WCAG22_FOCUS_VISIBLE,
+  WCAG22_POINTER_GESTURES,
+  WCAG22_POINTER_CANCELLATION,
+  WCAG22_LABEL_IN_NAME,
+  WCAG22_MOTION_ACTUATION,
+  WCAG22_LANGUAGE_OF_PAGE,
+  WCAG22_LANGUAGE_OF_PARTS,
+  WCAG22_ON_FOCUS,
+  WCAG22_ON_INPUT,
+  WCAG22_CONSISTENT_NAVIGATION,
+  WCAG22_CONSISTENT_IDENTIFICATION,
+  WCAG22_ERROR_IDENTIFICATION,
+  WCAG22_LABELS_OR_INSTRUCTIONS,
+  WCAG22_ERROR_SUGGESTION,
+  WCAG22_ERROR_PREVENTION_LEGAL_FINANCIAL_DATA,
+  WCAG22_PARSING,
+  WCAG22_NAME_ROLE_VALUE,
+  WCAG22_STATUS_MESSAGES,
+] as const;
+
+const enumGuard =
+  <T>(values: readonly T[]) =>
+  <T>(x: unknown): x is T =>
+    values.includes(x as never);
+
+// TODO: Support AAA levels
+export type WcagTest = (typeof WCAG22_AA_LEVEL)[number];
+
+export const isWcagTest = enumGuard(WCAG22_AA_LEVEL);
+
+export interface WcagAudit {
+  baseline: string;
+  date: string;
+  fail: WcagTest[];
+  pass: WcagTest[];
+  notApplicable: WcagTest[];
+  notTested: WcagTest[];
+  cannotTell: WcagTest[];
+}
+
+export const initialAudit: Partial<WcagAudit> = {
+  cannotTell: [],
+  fail: [],
+  notApplicable: [],
+  notTested: [
+    WCAG22_NON_TEXT_CONTENT,
+    WCAG22_AUDIO_ONLY_AND_VIDEO_ONLY_PRERECORDED,
+    WCAG22_CAPTIONS_PRERECORDED,
+    WCAG22_AUDIO_DESCRIPTION_OR_MEDIA_ALTERNATIVE_PRERECORDED,
+    WCAG22_CAPTIONS_LIVE,
+    WCAG22_AUDIO_DESCRIPTION_PRERECORDED,
+    WCAG22_INFO_AND_RELATIONSHIPS,
+    WCAG22_MEANINGFUL_SEQUENCE,
+    WCAG22_SENSORY_CHARACTERISTICS,
+    WCAG22_ORIENTATION,
+    WCAG22_IDENTIFY_INPUT_PURPOSE,
+    WCAG22_USE_OF_COLOR,
+    WCAG22_AUDIO_CONTROL,
+    WCAG22_CONTRAST_MINIMUM,
+    WCAG22_RESIZE_TEXT,
+    WCAG22_IMAGES_OF_TEXT,
+    WCAG22_REFLOW,
+    WCAG22_NON_TEXT_CONTRAST,
+    WCAG22_TEXT_SPACING,
+    WCAG22_CONTENT_ON_HOVER_OR_FOCUS,
+    WCAG22_KEYBOARD,
+    WCAG22_NO_KEYBOARD_TRAP,
+    WCAG22_CHARACTER_KEY_SHORTCUTS,
+    WCAG22_TIMING_ADJUSTABLE,
+    WCAG22_PAUSE_STOP_HIDE,
+    WCAG22_THREE_FLASHES_OR_BELOW_THRESHOLD,
+    WCAG22_BYPASS_BLOCKS,
+    WCAG22_PAGE_TITLED,
+    WCAG22_FOCUS_ORDER,
+    WCAG22_LINK_PURPOSE_IN_CONTEXT,
+    WCAG22_MULTIPLE_WAYS,
+    WCAG22_HEADINGS_AND_LABELS,
+    WCAG22_FOCUS_VISIBLE,
+    WCAG22_POINTER_GESTURES,
+    WCAG22_POINTER_CANCELLATION,
+    WCAG22_LABEL_IN_NAME,
+    WCAG22_MOTION_ACTUATION,
+    WCAG22_LANGUAGE_OF_PAGE,
+    WCAG22_LANGUAGE_OF_PARTS,
+    WCAG22_ON_FOCUS,
+    WCAG22_ON_INPUT,
+    WCAG22_CONSISTENT_NAVIGATION,
+    WCAG22_CONSISTENT_IDENTIFICATION,
+    WCAG22_ERROR_IDENTIFICATION,
+    WCAG22_LABELS_OR_INSTRUCTIONS,
+    WCAG22_ERROR_SUGGESTION,
+    WCAG22_ERROR_PREVENTION_LEGAL_FINANCIAL_DATA,
+    WCAG22_PARSING,
+    WCAG22_NAME_ROLE_VALUE,
+    WCAG22_STATUS_MESSAGES,
+  ],
+  pass: [],
+};
