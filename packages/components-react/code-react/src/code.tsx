8b23fec09499cb0cf2cc6c93d55fa3b7dfba0828
Robbert Broersma <git@robbertbroersma.nl>
2024-12-15 12:40:28 +0100

    feat: prevent Code translation and render left-to-right by default

--- packages/components-react/code-react/src/code.tsx
+++ packages/components-react/code-react/src/code.tsx
@@ -8,7 +8,7 @@ export const Code = forwardRef<HTMLElement, CodeProps>(function Code(props, forw
   const { children, className, ...restProps } = props;
 
   return (
-    <code className={clsx('nl-code', className)} {...restProps} ref={forwardedRef}>
+    <code dir="ltr" translate="no" className={clsx('nl-code', className)} {...restProps} ref={forwardedRef}>
       {children}
     </code>
   );
