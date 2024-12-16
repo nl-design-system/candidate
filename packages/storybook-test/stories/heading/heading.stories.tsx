e3acd9e585a9afa6415ca62e0acdf571b103ef69
Robbert Broersma <git@robbertbroersma.nl>
2024-12-16 21:01:04 +0100

    docs: improve Heading stories

--- packages/storybook-test/stories/heading/heading.stories.tsx
+++ packages/storybook-test/stories/heading/heading.stories.tsx
@@ -1,6 +1,9 @@
+/* eslint-disable react/no-unescaped-entities */
 import type { Meta, StoryObj } from '@storybook/react';
+import { ExampleBodyTextDecorator } from '@nl-design-system-candidate/storybook-shared/src/ExampleBodyTextDecorator';
 import packageJSON from '../../../components-react/heading-react/package.json';
 import { Heading } from '../../../components-react/heading-react/src/css';
+import { Paragraph } from '../../../components-react/paragraph-react/src/css';
 import componentMarkdown from '../../../docs/heading-docs/docs/component.md?raw';
 import tokens from '../../../tokens/heading-tokens/tokens.json';
 
@@ -28,6 +31,7 @@ const meta = {
     },
   },
   component: Heading,
+  decorators: ExampleBodyTextDecorator,
   parameters: {
     docs: {
       description: {
@@ -56,115 +60,146 @@ type Story = StoryObj<typeof meta>;
 export const HeadingLevel1: Story = {
   name: 'Heading 1',
   args: {
-    children: 'Knappe Koppen Van Droom naar Realiteit',
+    children: 'Civiel recht in Nederland',
     level: 1,
   },
-  parameters: {
-    status: { type: [] },
-  },
-};
-
-export const HeadingLevel1SmallViewport: Story = {
-  name: 'Heading 1 (small viewport)',
-  args: {
-    children: 'Knappe Koppen Van Droom naar Realiteit',
-    level: 1,
-  },
+  decorators: (Story) => (
+    <>
+      {Story()}
+      <Paragraph>
+        Civiel recht, burgerlijk recht of privaatrecht bestaat uit een materieel en een formeel gedeelte. Het materiële
+        gedeelte stelt regels ten aanzien van de verhoudingen tussen burgers onderling en tussen burgers en goederen.
+        Het formeel privaatrecht is het burgerlijk procesrecht: het geeft regels over de handhaving van de materiële
+        regels.
+      </Paragraph>
+    </>
+  ),
   globals: {
-    viewport: { value: 'phone' },
-    zoom: 1,
-  },
-  parameters: {
-    status: { type: [] },
-  },
-};
-
-export const HeadingLevel1Zoom400: Story = {
-  name: 'Heading 1 (zoom 400%)',
-  args: {
-    children: 'Knappe Koppen Van Droom naar Realiteit',
-    level: 1,
-  },
-  globals: {
-    viewport: { value: undefined },
-    zoom: 4,
-  },
-  parameters: {
-    status: { type: [] },
-  },
-};
-
-export const HeadingLevel1Arabic: Story = {
-  name: 'Heading 1 (Arabic)',
-  args: {
-    children: 'مثال',
-    dir: 'rtl',
-    lang: 'ar',
-    level: 1,
+    dir: 'ltr',
+    lang: 'nl',
+    title: 'Civiel Recht in Nederland',
   },
   parameters: {
     docs: {
       description: {
-        story:
-          'Het moet mogelijk zijn een andere taal en tekstrichting in te stellen voor een heading, met het `lang` en `dir` attribuut. Bijvoorbeeld de taal Arabisch met `lang="ar"` en de right-to-left textrichting (`dir="rtl"`)',
+        story: `Een Heading (level 1) met daarna een Paragraph met bijbehorende tekst.
+
+Bron: [Civiel recht (Nederlands) - Wikipedia](https://nl.wikipedia.org/wiki/Civiel_recht_(Nederland))`,
       },
     },
     status: { type: [] },
   },
 };
 
-export const HeadingLevel1German: Story = {
-  name: 'Heading 1 (German)',
-  args: {
-    children: 'Rechtsschutzversicherungsgesellschaften',
-    level: 1,
-  },
-  globals: {
-    lang: 'de',
-  },
-  parameters: {
-    status: { type: [] },
-  },
-};
-
 export const HeadingLevel2: Story = {
-  name: 'Heading 2',
+  name: 'Heading 2 in een koppenstructuur',
   args: {
-    children: 'Knappe Koppen Van Droom naar Realiteit',
+    children: 'Burgerlijk Wetboek',
     level: 2,
   },
+  decorators: (Story) => (
+    <>
+      <Heading level={1}>Civiel recht in Nederland</Heading>
+      {Story()}
+      <Paragraph>Wijziging(en) op nader te bepalen datum(s); laatste bekendgemaakt in 2018.</Paragraph>
+    </>
+  ),
+  globals: {
+    dir: 'ltr',
+    lang: 'nl',
+    title: 'Civiel Recht in Nederland (fragment uit het Burgerlijk Wetboek)',
+  },
   parameters: {
+    docs: {
+      description: {
+        story: `Een Heading (level 2) over "Burgerlijk Wetboek", die volgt op een hoofdkop, met daarna een Paragraph met bijbehorende tekst.`,
+      },
+    },
     status: { type: [] },
   },
 };
 
 export const HeadingLevel3: Story = {
-  name: 'Heading 3',
+  name: 'Heading 3 in een koppenstructuur',
   args: {
-    children: 'Knappe Koppen Van Droom naar Realiteit',
+    children: 'Boek 1, Personen- en familierecht',
     level: 3,
   },
+  decorators: (Story) => (
+    <>
+      <Heading level={1}>Civiel recht in Nederland</Heading>
+      <Heading level={2}>Burgerlijk Wetboek</Heading>
+      {Story()}
+      <Paragraph>Dit boek bestaat uit Titel 1 tot en met Titel 20.</Paragraph>
+    </>
+  ),
+  globals: {
+    dir: 'ltr',
+    lang: 'nl',
+    title: 'Civiel Recht in Nederland (fragment uit het Burgerlijk Wetboek)',
+  },
   parameters: {
+    docs: {
+      description: {
+        story: `Een Heading (level 3) over "Boek 1, Personen- en familierecht", die volgt op Headings level 1 en 2, met daarna een Paragraph met bijbehorende tekst.`,
+      },
+    },
     status: { type: [] },
   },
 };
 
 export const HeadingLevel4: Story = {
-  name: 'Heading 4',
+  name: 'Heading 4 in een koppenstructuur',
   args: {
-    children: 'Knappe Koppen Van Droom naar Realiteit',
+    children: 'Titel 5. Het Huwelijk',
     level: 4,
   },
+  decorators: (Story) => (
+    <>
+      <Heading level={1}>Civiel recht in Nederland</Heading>
+      <Heading level={2}>Burgerlijk Wetboek</Heading>
+      <Heading level={3}>Boek 1, Personen- en familierecht</Heading>
+      {Story()}
+      <Paragraph>
+        Dit hoofdstuk bestaat uit een Algemene bepaling, gevolgd door Afdeling 1 tot en met Afdeling 5.
+      </Paragraph>
+    </>
+  ),
+  globals: {
+    dir: 'ltr',
+    lang: 'nl',
+    title: 'Civiel Recht in Nederland (fragment uit het Burgerlijk Wetboek)',
+  },
   parameters: {
+    docs: {
+      description: {
+        story: `Een Heading (level 4) over "Titel 5. Het Huwelijk", die volgt op Headings level 1 tot en met 3, met daarna een Paragraph met bijbehorende tekst.`,
+      },
+    },
     status: { type: [] },
   },
 };
 
 export const HeadingLevel5: Story = {
-  name: 'Heading 5',
+  name: 'Heading 5 in een koppenstructuur',
   args: {
-    children: 'Knappe Koppen Van Droom naar Realiteit',
+    children: 'Afdeling 1. Vereisten tot het aangaan van een huwelijk',
     level: 5,
+  },
+  decorators: (Story) => (
+    <>
+      <Heading level={1}>Civiel recht in Nederland</Heading>
+      <Heading level={2}>Burgerlijk Wetboek</Heading>
+      <Heading level={3}>Boek 1, Personen- en familierecht</Heading>
+      <Heading level={4}>Titel 5. Het huwelijk</Heading>
+      {Story()}
+      <Paragraph>Dit hoofdstuk bestaat uit Artikel 31 tot en met Artikel 42.</Paragraph>
+    </>
+  ),
+  globals: {
+    dir: 'ltr',
+    lang: 'nl',
+    title: 'Civiel Recht in Nederland (fragment uit het Burgerlijk Wetboek)',
   },
   parameters: {
     status: { type: [] },
@@ -172,12 +207,151 @@ export const HeadingLevel5: Story = {
 };
 
 export const HeadingLevel6: Story = {
-  name: 'Heading 6',
+  name: 'Heading 6 in een koppenstructuur',
   args: {
-    children: 'Knappe Koppen Van Droom naar Realiteit',
+    children: 'Artikel 42',
     level: 6,
   },
+  decorators: (Story) => (
+    <>
+      <Heading level={1}>Civiel recht in Nederland</Heading>
+      <Heading level={2}>Burgerlijk Wetboek</Heading>
+      <Heading level={3}>Boek 1, Personen- en familierecht</Heading>
+      <Heading level={4}>Titel 5. Het huwelijk</Heading>
+      <Heading level={5}>Afdeling 1. Vereisten tot het aangaan van een huwelijk</Heading>
+      <Heading level={6}>{Story()}</Heading>
+      <Paragraph>
+        Zij die met elkander een huwelijk willen aangaan, mogen niet tegelijkertijd een geregistreerd partnerschap zijn
+        aangegaan.
+      </Paragraph>
+    </>
+  ),
+  globals: {
+    dir: 'ltr',
+    lang: 'nl',
+    title: 'Civiel Recht in Nederland (fragment uit het Burgerlijk Wetboek)',
+  },
   parameters: {
+    docs: {
+      description: {
+        story: `Bron: [Artikel 42 Burgerlijk Wetboek Boek 1](https://wetten.overheid.nl/jci1.3:c:BWBR0002656&boek=1&titeldeel=5&afdeling=1&artikel=42&z=2024-01-01&g=2024-01-01)`,
+      },
+    },
+    status: { type: [] },
+  },
+};
+
+export const HeadingMultiline: Story = {
+  name: 'Heading 1 met meerdere regels tekst',
+  args: {
+    children: (
+      <>
+        When the pawn hits the conflicts he thinks like a king
+        <br />
+        What he knows throws the blows when he goes to the fight
+        <br />
+        And he'll win the whole thing 'fore he enters the ring
+        <br />
+        There's no body to batter when your mind is your might
+        <br />
+        So when you go solo, you hold your own hand
+        <br />
+        And remember that depth is the greatest of heights
+        <br />
+        And if you know where you stand, then you know where to land
+        <br />
+        And if you fall it won't matter, cuz you'll know that you're right
+      </>
+    ),
+    level: 1,
+  },
+  decorators: (Story) => (
+    <>
+      {Story()}
+      <Paragraph>
+        "When the Pawn..." is the second studio album by the American singer-songwriter Fiona Apple. Released by Epic
+        Records in the United States on November 9, 1999, When the Pawn... was wholly written by Apple, with production
+        by Jon Brion.
+      </Paragraph>
+    </>
+  ),
+  globals: {
+    dir: 'ltr',
+    lang: 'en',
+    title: 'When the Pawn...',
+  },
+  parameters: {
+    docs: {
+      description: {
+        story: `Een Heading (level 1) met een tekst die met line breaks over meerder regels is verdeeld.
+
+Bron: [When the Pawn... - Wikipedia](https://en.wikipedia.org/wiki/When_the_Pawn...)`,
+      },
+    },
+    status: { type: [] },
+  },
+};
+
+export const HeadingWordBreak: Story = {
+  name: 'Heading 1 met meerdere regels tekst',
+  args: {
+    children: 'Burgerservicenummer is geen onderdeel van vreemdelingendocumenten zoals het vreemdelingenpaspoort',
+    level: 1,
+  },
+  decorators: (Story) => (
+    <>
+      {Story()}
+      <Paragraph>
+        Op uw vreemdelingen document staat wel uw V-nummer: onderaan de verblijfsvergunning staan de tien cijfers van uw
+        V-nummer. Dat nummer staat achter de letters VNR. Uw V-nummer staat ook in brieven die u van de IND ontvangt.
+      </Paragraph>
+    </>
+  ),
+  globals: {
+    dir: 'ltr',
+    lang: 'nl',
+    title: 'Burgerservicenummer en uw vreemdelingendocument',
+  },
+  parameters: {
+    docs: {
+      description: {
+        story:
+          'Een Heading (level 1) met lange woorden die niet op 1 regel passen, die door CSS zijn afgebroken met koppelstreepjes.',
+      },
+    },
+    status: { type: [] },
+  },
+};
+
+export const HeadingArabic: Story = {
+  name: 'Heading met Arabisch schrift in een Nederlandstalige pagina',
+  args: {
+    children: 'عيد الفطر',
+    dir: 'rtl',
+    lang: 'ar',
+    level: 1,
+  },
+  decorators: (Story) => (
+    <>
+      {Story()}
+      <Paragraph>
+        Het Suikerfeest, Ramadanfeest of Eid-al-fitr (einde ramadan, Arabisch: عيد الفطر) is een islamitische feestdag
+        waarop het einde van de vastenmaand ramadan gevierd wordt.
+      </Paragraph>
+    </>
+  ),
+  globals: {
+    dir: 'ltr',
+    lang: 'nl',
+  },
+  parameters: {
+    docs: {
+      description: {
+        story: `Bron: [Suikerfeest - Wikipedia](https://nl.wikipedia.org/wiki/Suikerfeest)
+
+Het moet mogelijk zijn een andere taal en tekstrichting in te stellen voor een heading, met het \`lang\` en \`dir\` attribuut. Bijvoorbeeld de taal Arabisch met \`lang="ar"\` en de right-to-left textrichting met \`dir="rtl"\`.`,
+      },
+    },
     status: { type: [] },
   },
 };
@@ -195,7 +369,7 @@ export const HeadingLevelColors: Story = {
       },
     },
   },
-  render: ({ children = 'Knappe Koppen Van Droom naar Realiteit' }) => (
+  render: () => (
     <div
       style={{
         '--nl-heading-level-1-color': 'midnightblue',
@@ -213,21 +387,25 @@ export const HeadingLevelColors: Story = {
         backgroundColor: 'white',
       }}
     >
-      <Heading level={1}>{children}</Heading>
-      <Heading level={2}>{children}</Heading>
-      <Heading level={3}>{children}</Heading>
-      <Heading level={4}>{children}</Heading>
-      <Heading level={5}>{children}</Heading>
-      <Heading level={6}>{children}</Heading>
+      <Heading level={1}>Civiel recht in Nederland</Heading>
+      <Heading level={2}>Burgerlijk Wetboek</Heading>
+      <Heading level={3}>Boek 1, Personen- en familierecht</Heading>
+      <Heading level={4}>Titel 5. Het huwelijk</Heading>
+      <Heading level={5}>Afdeling 1. Vereisten tot het aangaan van een huwelijk</Heading>
+      <Heading level={6}>Artikel 42</Heading>
+      <Paragraph>
+        Zij die met elkander een huwelijk willen aangaan, mogen niet tegelijkertijd een geregistreerd partnerschap zijn
+        aangegaan.
+      </Paragraph>
     </div>
   ),
 };
 
 export const HeadingLevelSize: Story = {
-  name: 'Heading lettergrootte en line-height',
+  name: 'Heading met visueel design voor lettergrootte en line-height per heading level',
   args: {
-    children: 'Knappe Koppen Van Droom naar Realiteit',
-    level: 6,
+    children: '',
+    level: 1,
   },
   parameters: {
     docs: {
@@ -236,7 +414,7 @@ export const HeadingLevelSize: Story = {
       },
     },
   },
-  render: ({ children = 'Knappe Koppen Van Droom naar Realiteit' }) => (
+  render: () => (
     <div
       style={{
         '--nl-heading-level-1-font-size': '4em',
@@ -253,12 +431,16 @@ export const HeadingLevelSize: Story = {
         '--nl-heading-level-6-line-height': '1.8',
       }}
     >
-      <Heading level={1}>{children}</Heading>
-      <Heading level={2}>{children}</Heading>
-      <Heading level={3}>{children}</Heading>
-      <Heading level={4}>{children}</Heading>
-      <Heading level={5}>{children}</Heading>
-      <Heading level={6}>{children}</Heading>
+      <Heading level={1}>Civiel recht in Nederland</Heading>
+      <Heading level={2}>Burgerlijk Wetboek</Heading>
+      <Heading level={3}>Boek 1, Personen- en familierecht</Heading>
+      <Heading level={4}>Titel 5. Het huwelijk</Heading>
+      <Heading level={5}>Afdeling 1. Vereisten tot het aangaan van een huwelijk</Heading>
+      <Heading level={6}>Artikel 42</Heading>
+      <Paragraph>
+        Zij die met elkander een huwelijk willen aangaan, mogen niet tegelijkertijd een geregistreerd partnerschap zijn
+        aangegaan.
+      </Paragraph>
     </div>
   ),
 };
