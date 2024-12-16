62ed69b439e2664fc2b325a0582a4c76bdb83b19
Robbert <mail@robbertbroersma.nl>
2024-12-16 13:50:26 +0100

    docs: log a11y test findings with links to NL Design System (#380)
    Er zijn links nu losse URLs waar je stories kan testen, zoals
    bijvoorbeeld:
    
    -
    https://candidate-storybook-test-git-docs-wcag-links-nl-design-system.vercel.app/iframe.html?id=componenten-paragraph--paragraph-lead-arabic&args=&globals=&viewMode=story
    
    Deze pagina's hebben nu `<html lang=".."
    dir="..."><head><title>...</title>` zodat ook Language of the Page and
    Page Titled succesvol getest kunnen worden.
    
    Elke story kan nu een eigen toegankelijkheidsverklaring hebben, door een
    `wcagAudit` onder de `parameters` van een Story. Die wordt samengevoegd
    met de `wcagAudit` van de `meta`, zodat je niet alles bij elke test
    hoeft te herhalen.
    
    De audit bij een Story kan een eigen tester hebben, met een eigen
    testdatum.
    
    
    Algemeen gedeelte:
    
    <img width="1031" alt="Screenshot 2024-12-16 at 00 01 38"
    src="https://github.com/user-attachments/assets/4f0424b2-5518-4855-bee4-e5253189a158"
    />
    
    Per Story:
    
    <img width="976" alt="Screenshot 2024-12-15 at 23 59 04"
    src="https://github.com/user-attachments/assets/b2776b2a-9e45-41a3-bdf0-439f99027cdb"
    />

--- packages/storybook-shared/src/StoryRootDecorator.tsx
+++ packages/storybook-shared/src/StoryRootDecorator.tsx
@@ -1,10 +1,34 @@
 import type { Decorator } from '@storybook/react';
-import type { CSSProperties } from 'react';
+import { useEffect, type CSSProperties } from 'react';
 
 export const StoryRootDecorator: Decorator = (Story, context) => {
-  const { lang, storyRootClassname, zoom } = context.globals;
+  const { dir, lang, storyRootClassname, title, zoom } = context.globals;
   const style: CSSProperties = zoom ? { zoom } : {};
-  const dir = lang === 'ar' ? 'rtl' : undefined;
+
+  // In Storybook there are different view modes. When looking at all stories,
+  // the url is something like: `iframe.html?viewMode=docs&...`
+  //
+  // When looking at one just one Story, the URL is like: `iframe.html?viewMode=docs&...`
+  //
+  // Here we will recongnize such an URL, and render the Story settings on page level,
+  // to conform to the following WCAG 2.2 Success criteria:
+  //
+  // - Page title: https://nldesignsystem.nl/wcag/2.4.2
+  // - Language of the page: https://nldesignsystem.nl/wcag/3.1.1
+  const viewMode = typeof location !== 'undefined' ? new URLSearchParams(location.search).get('viewMode') : '';
+
+  useEffect(() => {
+    if (viewMode === 'story' && typeof document !== 'undefined') {
+      document.title = title || context.name;
+      document.documentElement.lang = lang;
+      document.documentElement.dir = dir || '';
+    } else if (viewMode === 'docs') {
+      // Restore the title, dir and lang to sensible defaults
+      document.title = context.title;
+      document.documentElement.lang = 'nl';
+      document.documentElement.dir = 'ltr';
+    }
+  }, [context.name]);
 
   return (
     <div data-story-root className={storyRootClassname} lang={lang} dir={dir} style={style}>
