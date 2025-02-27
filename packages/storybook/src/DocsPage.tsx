8295cb2b59727d7de2a6584f573b04c7b60a8f70
matijs <matijs@gmail.com>
2025-02-27 10:05:26 +0100

    test: add comment

--- packages/storybook/src/DocsPage.tsx
+++ packages/storybook/src/DocsPage.tsx
@@ -1,6 +1,7 @@
 import { DocsHeader } from '@nl-design-system-candidate/storybook-shared/src/DocsHeader';
 import { Controls, Description, Stories, Subtitle, Title } from '@storybook/blocks';
 
+/* comment */
 export const DocsPage = () => (
   <>
     <DocsHeader />
