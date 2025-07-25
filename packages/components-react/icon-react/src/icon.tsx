48d14bb0d70dbfa52502f6ee220b15bfe0816ada
matijs <matijs@gmail.com>
2025-07-25 10:52:51 +0200

    feat(Icon): add Icon component
    Add Icon React component and unit tests.
    Add Icon CSS component
    Add Icon Storybook stories
    Add Icon Storybook test stories

---
+++ packages/components-react/icon-react/src/icon.tsx
@@ -0,0 +1,46 @@
+import type { HTMLAttributes } from 'react';
+import { forwardRef } from 'react';
+
+type Dir = HTMLAttributes<HTMLSpanElement>['dir'];
+
+export type IconProps =
+  | (Omit<HTMLAttributes<HTMLSpanElement>, 'role' | 'aria-hidden' | 'aria-label' | 'aria-labelledby' | 'dir'> & {
+      'aria-hidden'?: true;
+      'aria-label'?: never;
+      'aria-labelledby'?: never;
+      dir?: Dir;
+      role?: never;
+    })
+  | (Omit<HTMLAttributes<HTMLSpanElement>, 'role' | 'aria-hidden'> & {
+      'aria-hidden'?: never;
+      'aria-label': string;
+      'aria-labelledby'?: never;
+      dir?: Dir;
+      role: 'img';
+    })
+  | (Omit<HTMLAttributes<HTMLSpanElement>, 'role' | 'aria-hidden'> & {
+      'aria-hidden'?: never;
+      'aria-label'?: never;
+      'aria-labelledby': string;
+      dir?: Dir;
+      role: 'img';
+    });
+
+function hasRoleImg(props: IconProps): props is Extract<IconProps, { role: 'img' }> {
+  return props.role === 'img';
+}
+
+const cn = (...classes: Array<string | undefined | null>): string => classes.filter(Boolean).join(' ');
+
+export const Icon = forwardRef<HTMLSpanElement, IconProps>(function Icon(props, forwardedRef) {
+  const { children, className, ...restProps } = props;
+  const ariaHidden = hasRoleImg(props) ? undefined : (true as const);
+
+  return (
+    <span ref={forwardedRef} className={cn('nl-icon', className)} aria-hidden={ariaHidden} {...restProps}>
+      {children}
+    </span>
+  );
+});
+
+Icon.displayName = 'Icon';
---
+++ packages/storybook/stories/icon.stories.tsx
@@ -0,0 +1,63 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import packageJSON from '../../components-react/icon-react/package.json';
+import { Icon } from '../../components-react/icon-react/src/css';
+
+const meta = {
+  args: {
+    children: (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="24"
+        fill="none"
+        stroke="currentColor"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="2"
+        viewBox="0 0 24 24"
+      >
+        <path stroke="none" d="M0 0h24v24H0z" />
+        <path d="M3 12a9 9 0 1 0 18 0 9 9 0 1 0-18 0" />
+        <path d="m10 16.5 2-3 2 3m-2-3v-2l3-1m-6 0 3 1" />
+        <circle cx="12" cy="7.5" r=".5" fill="currentcolor" />
+      </svg>
+    ),
+  },
+  argTypes: {
+    'aria-label': { table: { category: 'API' }, type: 'string' },
+    'aria-labelledby': { table: { category: 'API' }, type: 'string' },
+    // Hide children as it's a React component which cannot be displayed nicely in the Storybook UI
+    children: { table: { disable: true } },
+    className: { table: { category: 'API' }, type: 'string' },
+    dir: {
+      control: { labels: { undefined: '(undefined)' }, type: 'select' },
+      options: [undefined, 'rtl', 'ltr', 'auto'],
+      table: { category: 'API' },
+    },
+    role: {
+      control: { labels: { undefined: '(undefined)' }, type: 'select' },
+      options: [undefined, 'img'],
+      table: { category: 'API' },
+    },
+  },
+  component: Icon,
+  parameters: {
+    externalLinks: [
+      {
+        name: 'Open op NL Design System',
+        url: 'https://nldesignsystem.nl/icon',
+      },
+      {
+        name: 'Open op GitHub',
+        url: packageJSON.homepage,
+      },
+    ],
+  },
+  title: 'Icon',
+} satisfies Meta<typeof Icon>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {};
