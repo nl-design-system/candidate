ceb8d98975f0e0f7f7ffdb5301c78633b9f58428
matijs <matijs@gmail.com>
2025-08-08 12:04:17 +0100

    refactor: number-badge-react
    Remove the clsx dependency and replace it with a oneliner equivalent to
    concatenate classNames.

--- packages/components-react/number-badge-react/src/number-badge.tsx
+++ packages/components-react/number-badge-react/src/number-badge.tsx
@@ -1,5 +1,4 @@
 import type { DataHTMLAttributes, ForwardedRef, HTMLAttributes, ReactNode } from 'react';
-import { clsx } from 'clsx';
 import { forwardRef } from 'react';
 
 interface NumberBadgePropsForData extends DataHTMLAttributes<HTMLDataElement> {
@@ -13,12 +12,14 @@ interface NumberBadgePropsForSpan extends HTMLAttributes<HTMLSpanElement> {
 
 const isNumberBadgePropsForData = (props: NumberBadgeProps): props is NumberBadgePropsForData => 'value' in props;
 
+const cn = (...classes: Array<string | undefined | null>): string => classes.filter(Boolean).join(' ');
+
 export type NumberBadgeProps = NumberBadgePropsForData | NumberBadgePropsForSpan;
 
 export const NumberBadge = forwardRef<HTMLDataElement | HTMLSpanElement, NumberBadgeProps>(
   function NumberBadge(props, ref) {
     const { children, label, ...restProps } = props;
-    const className = clsx('nl-number-badge', props.className);
+    const className = cn('nl-number-badge', props.className);
 
     /**
      * When CSS cannot be loaded, the labels must in the opposite way to make sure the meaning is clear without visual aids.
