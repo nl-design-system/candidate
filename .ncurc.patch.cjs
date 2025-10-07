d950ac20afe74b33f28d4ed7230626501510e80a
Richard van Heuven van Staereling <richadr@users.noreply.github.com>
2025-10-07 13:44:23 +0200

    build: configure a cooldown of 1 day for npm-check-updates

--- .ncurc.patch.cjs
+++ .ncurc.patch.cjs
@@ -1,4 +1,5 @@
 module.exports = {
+  cooldown: 1, // 1 day
   dep: ['dev', 'prod'],
   install: 'always',
   // Keep the contents of `reject` in line with "version-update:semver-patch" ignore entries in .github/dependabot.yml
