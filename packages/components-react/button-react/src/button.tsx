773baaa4f3cfed7c4d9f2173d75077d016a6d20a
Peter Goes <petergoes@gmail.com>
2025-10-21 16:33:14 +0200

    docs(button): add a11y unit tests (#671)

--- packages/components-react/button-react/src/button.tsx
+++ packages/components-react/button-react/src/button.tsx
@@ -1,16 +1,16 @@
-import type { HTMLAttributes, ReactNode } from 'react';
+import type { ButtonHTMLAttributes, ReactNode } from 'react';
 import { clsx } from 'clsx';
 import { forwardRef } from 'react';
 
-export interface ButtonProps extends HTMLAttributes<HTMLButtonElement> {
+export interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   iconEnd?: ReactNode;
   iconStart?: ReactNode;
 }
 
 export const Button = forwardRef<HTMLButtonElement, ButtonProps>(function Button(props, forwardedRef) {
-  const { children, className, ...restProps } = props;
+  const { children, className, type = 'button', ...restProps } = props;
   return (
-    <button className={clsx('nl-button', className)} ref={forwardedRef} {...restProps}>
+    <button type={type} className={clsx('nl-button', className)} ref={forwardedRef} {...restProps}>
       {children}
     </button>
   );
