2240c56a5b5db5f7560a98cba46c7924d111a276
Peter Goes <petergoes@gmail.com>
2025-11-21 09:53:04 +0100

    chore: move `icon` stories to `icon-docs` package

--- packages/storybook/stories/icon.stories.tsx
+++ packages/storybook/stories/icon.stories.tsx
@@ -1,47 +1,11 @@
-import type { Meta, StoryObj } from '@storybook/react-vite';
-import '../../components-css/icon-css/src/icon.scss';
+import type { Meta } from '@storybook/react-vite';
 import packageJSON from '../../components-react/icon-react/package.json';
-import { Icon } from '../../components-react/icon-react/src/icon';
+import { Icon } from '../../components-react/icon-react';
+import iconMeta from '@nl-design-system-candidate/icon-docs/stories/icon.react.meta';
+import * as Stories from '@nl-design-system-candidate/icon-docs/stories/icon.stories';
 
 const meta = {
-  args: {
-    children: (
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        width="24"
-        height="24"
-        fill="none"
-        stroke="currentColor"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        strokeWidth="2"
-        viewBox="0 0 24 24"
-      >
-        <path stroke="none" d="M0 0h24v24H0z" />
-        <path d="M3 12a9 9 0 1 0 18 0 9 9 0 1 0-18 0" />
-        <path d="m10 16.5 2-3 2 3m-2-3v-2l3-1m-6 0 3 1" />
-        <circle cx="12" cy="7.5" r=".5" fill="currentcolor" />
-      </svg>
-    ),
-  },
-  argTypes: {
-    'aria-label': { table: { category: 'API' }, type: 'string' },
-    'aria-labelledby': { table: { category: 'API' }, type: 'string' },
-    // Hide children as it's a React component which cannot be displayed nicely in the Storybook UI
-    children: { table: { disable: true } },
-    className: { table: { category: 'API' }, type: 'string' },
-    dir: {
-      control: { labels: { undefined: '(undefined)' }, type: 'select' },
-      options: [undefined, 'rtl', 'ltr', 'auto'],
-      table: { category: 'API' },
-    },
-    role: {
-      control: { labels: { undefined: '(undefined)' }, type: 'select' },
-      options: [undefined, 'img'],
-      table: { category: 'API' },
-    },
-  },
-  component: Icon,
+  ...iconMeta,
   parameters: {
     externalLinks: [
       {
@@ -59,6 +23,4 @@ const meta = {
 
 export default meta;
 
-type Story = StoryObj<typeof meta>;
-
-export const Default: Story = {};
+export const Default = Stories.Default;
