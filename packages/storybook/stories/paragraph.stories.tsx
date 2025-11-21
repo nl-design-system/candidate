9e5ac546f54bd320388992aacbc10c4ba036d85f
Peter Goes <petergoes@gmail.com>
2025-11-21 09:53:04 +0100

    chore: move `paragraph` stories to `paragraph-docs` package

--- packages/storybook/stories/paragraph.stories.tsx
+++ packages/storybook/stories/paragraph.stories.tsx
@@ -1,20 +1,18 @@
 import { ExampleBodyTextDecorator } from '@nl-design-system-candidate/storybook-shared/src/ExampleBodyTextDecorator';
-import type { Meta, StoryObj } from '@storybook/react-vite';
+import type { Meta } from '@storybook/react-vite';
 import '../../components-css/paragraph-css/src/paragraph.scss';
 import packageJSON from '../../components-react/paragraph-react/package.json';
 import { Paragraph } from '../../components-react/paragraph-react/src/paragraph';
+import paragraphMeta from '@nl-design-system-candidate/paragraph-docs/stories/paragraph.react.meta';
+import * as Stories from '@nl-design-system-candidate/paragraph-docs/stories/paragraph.stories';
 
 const meta = {
-  argTypes: {
-    children: { table: { category: 'API' } },
-    purpose: {
-      control: { labels: { undefined: '(undefined)' }, type: 'select' },
-      options: [undefined, 'lead'],
-      table: { category: 'API' },
-    },
-  },
-  component: Paragraph,
+  ...paragraphMeta,
   decorators: [ExampleBodyTextDecorator],
+  globals: {
+    dir: 'ltr',
+    lang: 'nl',
+  },
   parameters: {
     externalLinks: [
       {
@@ -32,28 +30,5 @@ const meta = {
 
 export default meta;
 
-type Story = StoryObj<typeof meta>;
-
-export const Default: Story = {
-  name: 'Paragraph',
-  args: {
-    children: 'Op brute wĳze ving de schooljuf de quasi-kalme lynx.',
-    purpose: undefined,
-  },
-  globals: {
-    dir: 'ltr',
-    lang: 'nl',
-  },
-};
-
-export const Lead: Story = {
-  name: 'Paragraph Lead',
-  args: {
-    children: 'Op brute wĳze ving de schooljuf de quasi-kalme lynx.',
-    purpose: 'lead',
-  },
-  globals: {
-    dir: 'ltr',
-    lang: 'nl',
-  },
-};
+export const Default = Stories.Default;
+export const Lead = Stories.Lead;
