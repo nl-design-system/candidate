e4f0270f93332d5118a1a9769604973793ebdf79
Peter Goes <petergoes@gmail.com>
2025-11-25 14:18:40 +0100

    chore(skip-link): move stories to docs package (#744)
    closes: #735
    
    ## How to test:
    De stories in: 
    
    https://candidate-git-chore-move-skip-link-stor-70c796-nl-design-system.vercel.app/?path=/docs/skip-link--documentatie
    
    
    moet gelijk zijn aan:
    
    https://nl-design-system.github.io/candidate/?path=/docs/skip-link--documentatie

--- packages/storybook/stories/skip-link.stories.tsx
+++ packages/storybook/stories/skip-link.stories.tsx
@@ -1,19 +1,11 @@
-import type { Meta, StoryObj } from '@storybook/react-vite';
-import '../../components-css/skip-link-css/src/skip-link.scss';
+import type { Meta } from '@storybook/react-vite';
 import packageJSON from '../../components-react/skip-link-react/package.json';
-import { SkipLink } from '../../components-react/skip-link-react/src/skip-link';
+import { SkipLink as SkipLinkComponent } from '../../components-react/skip-link-react/src/skip-link';
+import skipLinkMeta from '@nl-design-system-candidate/skip-link-docs/stories/skip-link.react.meta';
+import * as Stories from '@nl-design-system-candidate/skip-link-docs/stories/skip-link.stories';
 
 const meta = {
-  argTypes: {
-    children: { table: { category: 'API' }, type: 'string' },
-    href: { table: { category: 'API' } },
-    target: {
-      control: { labels: { undefined: '(undefined)' }, type: 'select' },
-      options: [undefined, '_blank', '_parent', '_self', '_top'],
-      table: { category: 'API' },
-    },
-  },
-  component: SkipLink,
+  ...skipLinkMeta,
   parameters: {
     externalLinks: [
       {
@@ -27,16 +19,8 @@ const meta = {
     ],
   },
   title: 'Skip Link',
-} satisfies Meta<typeof SkipLink>;
+} satisfies Meta<typeof SkipLinkComponent>;
 
 export default meta;
 
-type Story = StoryObj<typeof meta>;
-
-export const Default: Story = {
-  name: 'Skip Link',
-  args: {
-    children: 'Naar de inhoud',
-    href: '#inhoud',
-  },
-};
+export const SkipLink = Stories.SkipLink;
