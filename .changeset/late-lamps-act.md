fe1835e65d301c4ccf61a5d61d101c6745fa9625
Peter Goes <petergoes@gmail.com>
2025-11-25 15:15:28 +0100

    chore(heading): move stories to docs package (#738)
    closes: #729
    
    ## How to test:
    De stories in: 
    
    https://candidate-git-chore-move-heading-storie-b12a8e-nl-design-system.vercel.app/?path=/docs/heading--documentatie
    
    
    moet gelijk zijn aan:
    
    https://nl-design-system.github.io/candidate/?path=/docs/heading--documentatie

---
+++ .changeset/late-lamps-act.md
@@ -0,0 +1,5 @@
+---
+'@nl-design-system-candidate/heading-docs': minor
+---
+
+Stories toegevoegd als exports zodat deze kunnen worden hergebruikt
--- packages/storybook/stories/heading.stories.tsx
+++ packages/storybook/stories/heading.stories.tsx
@@ -1,25 +1,11 @@
-/* eslint-disable react/no-unescaped-entities */
-import { Paragraph } from '@nl-design-system-candidate/paragraph-react/css';
-import type { Meta, StoryObj } from '@storybook/react-vite';
-import '../../components-css/heading-css/src/heading.scss';
+import type { Meta } from '@storybook/react-vite';
 import packageJSON from '../../components-react/heading-react/package.json';
 import { Heading } from '../../components-react/heading-react/src/heading';
+import headingMeta from '@nl-design-system-candidate/heading-docs/stories/heading.react.meta';
+import * as Stories from '@nl-design-system-candidate/heading-docs/stories/heading.stories';
 
 const meta = {
-  argTypes: {
-    appearance: {
-      control: { labels: { undefined: '(undefined)' }, type: 'select' },
-      options: [undefined, 'level-1', 'level-2', 'level-3', 'level-4', 'level-5', 'level-6'],
-      table: { category: 'API' },
-    },
-    children: { table: { category: 'API' } },
-    level: {
-      control: { type: 'select' },
-      options: [1, 2, 3, 4, 5, 6],
-      table: { category: 'API' },
-    },
-  },
-  component: Heading,
+  ...headingMeta,
   parameters: {
     externalLinks: [
       {
@@ -37,63 +23,4 @@ const meta = {
 
 export default meta;
 
-type Story = StoryObj<typeof meta>;
-
-export const HeadingMultiline: Story = {
-  name: 'Heading 1 met meerdere regels tekst',
-  args: {
-    children: (
-      <>
-        When the pawn hits the conflicts he thinks like a king
-        <br />
-        What he knows throws the blows when he goes to the fight
-        <br />
-        And he'll win the whole thing 'fore he enters the ring
-        <br />
-        There's no body to batter when your mind is your might
-        <br />
-        So when you go solo, you hold your own hand
-        <br />
-        And remember that depth is the greatest of heights
-        <br />
-        And if you know where you stand, then you know where to land
-        <br />
-        And if you fall it won't matter, cuz you'll know that you're right
-      </>
-    ),
-    level: 1,
-  },
-  decorators: (Story) => (
-    <>
-      {Story()}
-      <Paragraph>
-        "When the Pawn..." is the second studio album by the American singer-songwriter Fiona Apple. Released by Epic
-        Records in the United States on November 9, 1999, When the Pawn... was wholly written by Apple, with production
-        by Jon Brion.
-      </Paragraph>
-      <Paragraph>
-        Upon its release, "When the Pawn..." broke the record for longest album title at 444 characters (previously held
-        by a volume in "The Best... Album in the World...Ever!"), though this record was subsequently broken.
-      </Paragraph>
-    </>
-  ),
-  globals: {
-    dir: 'ltr',
-    lang: 'en',
-    title: 'When the Pawn...',
-  },
-  parameters: {
-    docs: {
-      description: {
-        story: `Een Heading (level 1) met een tekst die met line breaks over meerder regels is verdeeld.
-
-Bron: [When the Pawn... - Wikipedia](https://en.wikipedia.org/wiki/When_the_Pawn...)
-
-Dit is een voorbeeld van een Heading met \`<br/>\` elementen, een situatie die soms voorkomt in de praktijk.
-
-Dit voorbeeld voldoet niet aan de best practices van NL Design System om de tekst van de Heading niet te lang te maken.`,
-      },
-    },
-    status: { type: [] },
-  },
-};
+export const Heading1MetMeerdereRegelsTekst = Stories.Heading1MetMeerdereRegelsTekst;
