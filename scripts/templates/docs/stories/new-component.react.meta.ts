025a873e47dc3260502ef4e3565d5238d61aca79
Peter Goes <petergoes@gmail.com>
2025-11-27 10:29:15 +0100

    chore(templates): create a description.md file for new `docs` packages

--- scripts/templates/docs/stories/new-component.react.meta.ts
+++ scripts/templates/docs/stories/new-component.react.meta.ts
@@ -1,9 +1,17 @@
 import type { Meta } from '@storybook/react-vite';
 import { NewComponent } from '@nl-design-system-candidate/new-component-react/css';
+import descriptionMarkdown from '../docs/description.md?raw';
 
 const meta = {
   argTypes: {
     children: { control: 'text', table: { category: 'API' } },
+  },
+  parameters: {
+    docs: {
+      description: {
+        component: descriptionMarkdown,
+      },
+    },
   },
   component: NewComponent,
 } satisfies Meta<typeof NewComponent>;
