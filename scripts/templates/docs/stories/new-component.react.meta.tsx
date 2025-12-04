86071551578e87d23919c9b7abb4c8b27adb0aeb
Peter Goes <petergoes@gmail.com>
2025-12-04 15:34:16 +0100

    chore(templates): remove description from react.meta.tsx

--- scripts/templates/docs/stories/new-component.react.meta.tsx
+++ scripts/templates/docs/stories/new-component.react.meta.tsx
@@ -1,17 +1,9 @@
 import type { Meta } from '@storybook/react-vite';
 import { NewComponent } from '@nl-design-system-candidate/new-component-react/css';
-import descriptionMarkdown from '../docs/description.md?raw';
 
 const meta = {
   argTypes: {
     children: { control: 'text', table: { category: 'API' } },
-  },
-  parameters: {
-    docs: {
-      description: {
-        component: descriptionMarkdown,
-      },
-    },
   },
   component: NewComponent,
 } satisfies Meta<typeof NewComponent>;
