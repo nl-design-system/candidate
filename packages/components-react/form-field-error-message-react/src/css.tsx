74ad396795d61a6cd74fc96bf2ddbc125f923402
Charlotte <42849307+eslook@users.noreply.github.com>
2026-02-10 16:09:45 +0100

    docs: form field error message - keywords en aliassen bepaald - prepare packages and stories (#1044)
    Onderdeel van https://github.com/nl-design-system/candidate/issues/913
    
    ---------
    
    Co-authored-by: Charlotte Gieltjes <charlotte.gieltjes@rebels.io>

---
+++ packages/components-react/form-field-error-message-react/src/css.tsx
@@ -0,0 +1,3 @@
+import '@nl-design-system-candidate/form-field-error-message-css/form-field-error-message.css';
+
+export { FormFieldErrorMessage } from './form-field-error-message';
---
+++ packages/docs/form-field-error-message-docs/README.md
@@ -0,0 +1,6 @@
+<!-- @license CC0-1.0 -->
+
+# Form Field Error Message documentatie
+
+- [anatomie](./docs/anatomy/anatomy.md)
+- [aliassen](./docs/aliases.md)
