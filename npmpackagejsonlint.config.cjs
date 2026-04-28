5e328372786b45f5c080df39b08fb6b5788d7b9f
Richard van Heuven van Staereling <richadr@users.noreply.github.com>
2026-04-28 14:13:30 +0200

    chore: require provenance for published package

--- npmpackagejsonlint.config.cjs
+++ npmpackagejsonlint.config.cjs
@@ -7,12 +7,39 @@ module.exports = {
     'no-tilde-version-dependencies': 'error',
     'no-tilde-version-devDependencies': 'error',
     'require-repository-directory': 'error',
+    'require-publishConfig': 'error',
+    'valid-values-publishConfig': [
+      'error',
+      [
+        {
+          access: 'public',
+          provenance: true,
+        },
+        {
+          access: 'restricted',
+          provenance: true,
+        },
+      ],
+    ],
   },
   overrides: [
     {
       patterns: ['packages/docs/**/package.json', 'scripts/templates/docs/package.json'],
       rules: {
         'valid-values-license': ['error', ['CC0-1.0']],
+      },
+    },
+    {
+      // These packages are not published to npm, so we don't need to require publishConfig
+      patterns: [
+        './package.json',
+        'packages/storybook/package.json',
+        'packages/storybook-non-conforming/package.json',
+        'packages/storybook-shared/package.json',
+        'packages/storybook-test/package.json',
+      ],
+      rules: {
+        'require-publishConfig': 'off',
       },
     },
   ],
