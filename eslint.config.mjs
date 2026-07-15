33f8655da802c4ba81300520bb673eefe5207943
Ali Amori Kadhim <ali@frameless.io>
2026-07-15 15:33:18 +0200

    test: add Playwright end-to-end tests for Button component

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,1 +1,7 @@
-export { default } from '@nl-design-system/eslint-config';
+import nlDesignSystemConfig from '@nl-design-system/eslint-config';
+import { globalIgnores } from 'eslint/config';
+
+export default [
+  ...nlDesignSystemConfig,
+  globalIgnores(['**/playwright-report/', '**/test-results/', '**/e2e/playwright/.cache/']),
+];
