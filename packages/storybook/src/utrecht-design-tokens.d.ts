0eee631d71d5956dd8a36521c6860906f97fd578
Ali Amori Kadhim <ali@frameless.io>
2026-09-14 10:30:48 +0200

    fix: correct the design-tokens.d.ts file name

--- packages/storybook/src/utrecht-design-tokens.d.ts
+++
@@ -1,7 +0,0 @@
-import 'react';
-
-declare module 'react' {
-  export interface CSSProperties {
-    [key: `--${'example' | 'denhaag' | 'utrecht' | 'nl'}-${string}`]: string | number | undefined;
-  }
-}
