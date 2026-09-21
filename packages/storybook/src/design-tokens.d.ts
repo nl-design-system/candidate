c380df2fb5f99b65e70decc5c41c9ad373cfaef4
Ali Amori Kadhim <ali@frameless.io>
2026-09-21 13:45:30 +0200

    docs: move color-sample stories to storybook-test package

--- packages/storybook/src/design-tokens.d.ts
+++
@@ -1,7 +0,0 @@
-import 'react';
-
-declare module 'react' {
-  export interface CSSProperties {
-    [key: `--${'example' | 'nl'}-${string}`]: string | number | undefined;
-  }
-}
--- packages/storybook/stories/color-sample.css.stories.tsx
+++ packages/storybook/stories/color-sample.css.stories.tsx
@@ -1,26 +1,12 @@
 import type { Meta } from '@storybook/react-vite';
 import packageJSON from '../../components-css/color-sample-css/package.json';
-import {
-  ColorSample as ColorSampleComponent,
-  type ColorSampleProps,
-} from '@nl-design-system-candidate/color-sample-react';
+import { ColorSample as ColorSampleComponent } from '@nl-design-system-candidate/color-sample-react';
 import colorSampleMeta from '@nl-design-system-candidate/color-sample-docs/stories/color-sample.css.meta';
 import * as Stories from '@nl-design-system-candidate/color-sample-docs/stories/color-sample.stories';
 import { mergeCssMeta } from '../src/helpers/merge-css-meta';
 import { getExternalLinks } from '../src/helpers/external-links';
 
 const externalLinks = getExternalLinks('https://nldesignsystem.nl/color-sample', packageJSON.homepage);
-const DemoSvg = ({ style }: { style?: CSSProperties }) => (
-  <svg
-    className="nl-color-sample"
-    xmlns="http://www.w3.org/2000/svg"
-    viewBox="0 0 32 32"
-    preserveAspectRatio="none"
-    style={style}
-  >
-    <path d="M0 0H32V32H0Z" fill="currentColor" />
-  </svg>
-);
 
 const meta = {
   ...mergeCssMeta(colorSampleMeta, externalLinks),
