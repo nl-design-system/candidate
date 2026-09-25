72fd1f342ac0c484bb613201f6c01e180ed1c04a
Charlotte Gieltjes <charlotte.gieltjes@rebels.io>
2026-09-25 11:44:43 +0200

    docs: feedback and learnings from form field label tests

--- packages/components-react/form-field-label-react/src/form-field-label.test.tsx
+++ packages/components-react/form-field-label-react/src/form-field-label.test.tsx
@@ -11,45 +11,17 @@ afterEach(() => {
 });
 
 describe('Form Field Label', () => {
-  describe('Use Cases', () => {
-    it.todo('supports phrasing content', () => {});
-    it.todo('supports nesting the associated form control instead of using `for` HTML-attribute', () => {});
-  });
-
-  describe('HTML API', () => {
-    it.todo('renders a `label` HTML-element', () => {});
-    it.todo('supports the `for` HTML-attribute', () => {});
-    it.todo('supports the `hidden` HTML-attribute', () => {});
-    it.todo('supports the `lang` HTML-attribute', () => {});
-    it.todo('supports the `dir` HTML-attribute', () => {});
-  });
-
   describe('CSS API', () => {
-    it.todo('adds `nl-form-field-label` class by default', () => {});
-    it.todo('adds `nl-form-field-label--option` class when `type="option"`', () => {});
-    it.todo('adds `nl-form-field-label--inherit` class when `type="inherit"`', () => {});
-    it.todo('adds `nl-form-field-label--disabled` class when `disabled` is true', () => {});
-  });
-
-  describe('Property: Type', () => {
-    it.todo('supports `type="option"`', () => {});
-    it.todo('supports `type="inherit"`', () => {});
-  });
-
-  describe('Slot: Input', () => {
-    it.todo('supports an input slot for radio', () => {});
-    it.todo('supports an input slot for checkbox', () => {});
-    it.todo('supports an input slot for switch', () => {});
-    it.todo('requires `type="option"` when an input slot is used', () => {});
-    it.todo('requires an input slot when `type="option"` is used', () => {});
+    it.todo('adds the `nl-form-field-label` class by default', () => {});
   });
 
   describe('Component API', () => {
-    it.todo('supports forwarding HTML-attributes to the underlying `label`', () => {});
-    it.todo('supports the `for` HTML-attribute', () => {});
-    it.todo('supports the `hidden` HTML-attribute', () => {});
-    it.todo('supports the `lang` HTML-attribute', () => {});
-    it.todo('supports the `dir` HTML-attribute', () => {});
+    it.todo('renders the HTML-element `label`', () => {});
+    it.todo('supports forwarding the HTML-attribute `hidden` to the underlying HTML-element `label`', () => {});
+    it.todo('supports forwarding the HTML-attribute `lang` to the underlying HTML-element `label`', () => {});
+    it.todo('supports forwarding the HTML-attribute `dir` to the underlying HTML-element `label`', () => {});
+    it.todo('supports forwarding the HTML-attribute `for` to the underlying HTML-element `label`', () => {});
+    it.todo('supports phrasing content', () => {});
   });
 
   describe('React API', () => {
@@ -58,12 +30,48 @@ describe('Form Field Label', () => {
     });
 
     it.todo('forwards React refs to the HTMLLabelElement', () => {
-      const ref = createRef<HTMLDivElement>();
+      const ref = createRef<HTMLLabelElement>();
       render(<FormFieldLabel ref={ref}>form-field-label</FormFieldLabel>);
       const element = screen.getByText('form-field-label');
 
       expect(ref.current).toBe(element);
-      expect(element).toBeInstanceOf(HTMLDivElement);
+      expect(element).toBeInstanceOf(HTMLLabelElement);
+    });
+  });
+
+  describe('property `disabled`', () => {
+    describe('CSS API', () => {
+      it.todo('adds the `nl-form-field-label--disabled` class', () => {});
+    });
+  });
+
+  describe('property `type`', () => {
+    describe('with value "option"', () => {
+      describe('CSS API', () => {
+        it.todo('adds the `nl-form-field-label--inherit` class', () => {});
+      });
+    });
+
+    describe('with value "inherit"', () => {
+      it.todo('supports `type="inherit"`', () => {});
+
+      describe('CSS API', () => {
+        it.todo('adds the `nl-form-field-label--inherit` class', () => {});
+      });
+    });
+  });
+
+  describe('Slot `input`', () => {
+    describe('when `type` is not `option`', () => {
+      describe('Component API', () => {
+        it.todo('does not render children', () => {});
+      });
+    });
+
+    describe('when `type` is `option`', () => {
+      describe('Component API', () => {
+        it.todo('renders children', () => {});
+      });
     });
   });
 });
