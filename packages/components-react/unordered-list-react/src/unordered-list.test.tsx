b7b626d64ed0a8971a1702ea5ed371783fe4bc4b
Charlotte Gieltjes <charlotte.gieltjes@rebels.io>
2026-09-25 07:39:18 +0200

    docs: add all Unordered List stories specs and testcases for draft PR
    Onderdeel van https://github.com/nl-design-system/candidate/issues/1189 en https://github.com/nl-design-system/candidate/issues/1188
    
    Bevat alle stories, test stories, evil stories en tests voor de component. In de GitHub Pull Request werken we samen om deze in te vullen en vervolgens te reviewen. Deze zijn dummycode, zodat ze voorbereid zijn om in de
    Ontwikkelfase daadwerkelijk uitgewerkt te krijgen.
    
    ---
    
    Voor reviewers:
    
    We verzamelen nu specifiek feedback op de stories in de storybestanden, de tests mag je buiten beschouwing laten.
    
    - De naam: Is die beschrijvend genoeg?
    - De beschrijving: Begrijp je het? Is het te technisch? Of mis je juist informatie nu? Vind je het taalgebruik toegankelijk voor iedereen?
    - Mis je stories?
    
    Reviews zijn al uitgevoerd door (middels dezelfde review op Ordered List)
    - [x] toegankelijkheidsexpert
    - [x] designer woensdag (jeff)
    - [x] designer donderdag (roos)
    - [x] developer (algemeen)
    - [x] developer (ontwikkelfase)
    
    Ready voor Ontwikkelfase developer; alles duidelijk om te bouwen
    - [x] Ja, is gecontroleerd en geOK'd
    
    Ready voor Design System Lead?
    - [x] Ja
    - [ ] Nee, nog niet

--- packages/components-react/unordered-list-react/src/unordered-list.test.tsx
+++ packages/components-react/unordered-list-react/src/unordered-list.test.tsx
@@ -10,17 +10,118 @@ afterEach(() => {
   cleanup();
 });
 
-describe('UnorderedList', () => {
-  it(`has displayName "${displayName}"`, () => {
-    expect(UnorderedList.displayName).toBe(displayName);
+describe('Unordered List', () => {
+  describe('CSS API', () => {
+    it.todo('adds `nl-unordered-list` class by default', () => {});
   });
 
-  it('forwards React refs to the HTMLButtonElement', () => {
-    const ref = createRef<HTMLDivElement>();
-    render(<UnorderedList ref={ref}>unordered-list</UnorderedList>);
-    const element = screen.getByText('unordered-list');
+  describe('Component API', () => {
+    it.todo('renders the HTML-element `ul`', () => {});
+    it.todo('has the HTML-attribute `role="list"`', () => {});
+    it.todo('supports forwarding the HTML-attribute `hidden` to the underlying HTML-element `ul`', () => {});
+    it.todo('supports forwarding the HTML-attribute `lang` to the underlying HTML-element `ul`', () => {});
+    it.todo('supports forwarding the HTML-attribute `dir` to the underlying HTML-element `ul`', () => {});
+    it.todo('supports forwarding the HTML-attribute `role` to the underlying HTML-element `ul`', () => {});
+    it.todo('supports forwarding the HTML-attribute `reversed` to the underlying HTML-element `ul`', () => {});
+    it.todo('supports forwarding the HTML-attribute `start` to the underlying HTML-element `ul`', () => {});
+    it.todo('supports forwarding the HTML-attribute `type` to the underlying HTML-element `ul`', () => {});
+    it.todo(
+      'supports forwarding the HTML-attribute `type` with value `1` to the underlying HTML-element `ul`',
+      () => {},
+    );
+    it.todo(
+      'supports forwarding the HTML-attribute `type` with value `a` to the underlying HTML-element `ul`',
+      () => {},
+    );
+    it.todo(
+      'supports forwarding the HTML-attribute `type` with value `A` to the underlying HTML-element `ul`',
+      () => {},
+    );
+    it.todo(
+      'supports forwarding the HTML-attribute `type` with value `i` to the underlying HTML-element `ul`',
+      () => {},
+    );
+    it.todo(
+      'supports forwarding the HTML-attribute `type` with value `I` to the underlying HTML-element `ul`',
+      () => {},
+    );
+  });
 
-    expect(ref.current).toBe(element);
-    expect(element).toBeInstanceOf(HTMLDivElement);
+  describe('React API', () => {
+    it.todo(`has displayName "${displayName}"`, () => {
+      expect(UnorderedList.displayName).toBe(displayName);
+    });
+
+    it.todo('forwards React refs to the HTMLUListElement', () => {
+      const ref = createRef<HTMLUListElement>();
+      render(<UnorderedList ref={ref}>unordered-list</UnorderedList>);
+      const element = screen.getByText('unordered-list');
+
+      expect(ref.current).toBe(element);
+      expect(element).toBeInstanceOf(HTMLUListElement);
+    });
+  });
+});
+
+describe('Unordered List Item', () => {
+  describe('CSS API', () => {
+    it.todo('adds the `nl-unordered-list__item` class by default', () => {});
+  });
+
+  describe('Component API', () => {
+    it.todo('renders the HTML-element `li`', () => {});
+    it.todo('supports forwarding the HTML-attribute `hidden` to the underlying HTML-element `li`', () => {});
+    it.todo('supports forwarding the HTML-attribute `lang` to the underlying HTML-element `li`', () => {});
+    it.todo('supports forwarding the HTML-attribute `dir` to the underlying HTML-element `li`', () => {});
+    it.todo('supports phrasing content', () => {});
+    it.todo('supports the HTML-element `p`', () => {});
+  });
+});
+
+// Hint voor Ontwikkelfase developer, comment mag weg na ontwikkeling:
+// <ol class="nl-unordered-list" role="list">
+//   <li class="nl-unordered-list__item">
+//     <span class="nl-unordered-list__marker nl-unordered-list__marker--custom">
+//       <span aria-hidden="true">
+//        <Icon />
+//      </span>
+//      <span class="sr-only">Informatieve label{" "}</span>
+//     </span>
+//     Foo Bar
+//   </li>
+// </ol>
+describe('Unordered List Custom Marker when `marker` is not provided', () => {
+  describe('Component API', () => {
+    it.todo('does not render the HTML-element `span`', () => {});
+    it.todo('does not render the the custom marker HTML-element `span`', () => {});
+    it.todo('does not render the screenreader text HTML-element `span`', () => {});
+  });
+});
+
+describe('Unordered List Custom Marker when `marker` is provided', () => {
+  describe('and when `markerLabel` is not provided', () => {
+    it.todo('does not render the HTML-element `span`', () => {});
+    it.todo('does not render the the custom marker HTML-element `span`', () => {});
+    it.todo('does not render the screenreader text HTML-element `span`', () => {});
+  });
+
+  describe('and when `markerLabel` is provided', () => {
+    describe('CSS API', () => {
+      it.todo('adds the `nl-unordered-list__marker` class by default', () => {});
+      it.todo('adds the `nl-unordered-list__marker--custom` class by default', () => {});
+    });
+
+    describe('Component API', () => {
+      it.todo('renders the HTML-element `span`', () => {});
+
+      it.todo('renders the custom marker HTML-element `span`', () => {});
+      it.todo('has the HTML-attribute `aria-hidden="true"` on the custom marker HTML-element `span`', () => {});
+      it.todo('supports phrasing content in the custom marker HTML-element `span`', () => {});
+      it.todo('supports the HTML-element `p` in the custom marker HTML-element `span`', () => {});
+
+      it.todo('renders the screenreader text HTML-element `span`', () => {});
+      it.todo('supports phrasing content in the screenreader text HTML-element `span`', () => {});
+      it.todo('has the `sr-only` class on the screenreader text HTML-element `span`', () => {});
+    });
   });
 });
--- packages/storybook/stories/unordered-list.css.stories.tsx
+++ packages/storybook/stories/unordered-list.css.stories.tsx
@@ -2,7 +2,7 @@ import type { Meta } from '@storybook/react-vite';
 import packageJSON from '../../components-css/unordered-list-css/package.json';
 import { UnorderedList as UnorderedListComponent } from '@nl-design-system-candidate/unordered-list-react';
 import unorderedListMeta from '@nl-design-system-candidate/unordered-list-docs/stories/unordered-list.css.meta';
-// import * as Stories from '@nl-design-system-candidate/unordered-list-docs/stories/unordered-list.stories';
+import * as Stories from '@nl-design-system-candidate/unordered-list-docs/stories/unordered-list.stories';
 import { mergeCssMeta } from '../src/helpers/merge-css-meta.js';
 import { getExternalLinks } from '../src/helpers/external-links.js';
 
@@ -16,4 +16,4 @@ const meta = {
 
 export default meta;
 
-// export const UnorderedList = Stories.UnorderedList;
+export const UnorderedList = Stories.UnorderedList;
